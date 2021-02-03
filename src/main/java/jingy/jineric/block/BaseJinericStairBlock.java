84b16ecfc0c332f7e76760b5e7c56f973aa5d3df
Eric <69827559+Jiingy@users.noreply.github.com>
2021-02-02 22:53:51 -0800

    Organized classes Added soul_sandstone custom sounds (still WIP) Removed 'com' directory as I do not own the domain

---
+++ src/main/java/jingy/jineric/block/BaseJinericStairBlock.java
@@ -0,0 +1,10 @@
+package jingy.jineric.block;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.StairsBlock;
+
+class BaseJinericStairBlock extends StairsBlock {
+    public BaseJinericStairBlock (BlockState baseBlockState, Settings settings) {
+        super(baseBlockState, settings);
+    }
+}
