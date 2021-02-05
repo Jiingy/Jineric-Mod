ed21004caa73803d49f658978e506c75d47f4e15
Anthony <mrcheeze12345@gmail.com>
2021-02-04 18:07:43 -0700

    Revert "In-progress addition of Shulker Chest"
    This reverts commit dd9167178ecbb9b7259c8ba012275eb8f3ccd863.

--- src/main/java/jingy/jineric/Jineric.java
+++ src/main/java/jingy/jineric/Jineric.java
@@ -1,45 +1,30 @@
 package jingy.jineric;
 
-import jingy.jineric.blocks.JinericBlocks;
-import jingy.jineric.items.JinericItemGroups;
-import jingy.jineric.items.JinericItems;
-import jingy.jineric.sounds.JinericSounds;
+import jingy.jineric.block.JinericShulkerChestBlockEntity;
+import jingy.jineric.block.JinericBlocks;
+import jingy.jineric.block.JinericShulkerChestBlock;
+import jingy.jineric.item.JinericItemGroups;
+import jingy.jineric.item.JinericItems;
 import jingy.jineric.world.JinericWorldGen;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.container.ContainerProviderRegistry;
-import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.Util;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 
 
 public class Jineric implements ModInitializer {
-
     public static final String MOD_ID = "jineric";
-    public static final BlockEntityType<?> SHULKER_CHEST_ENTITY_TYPE = ;
 
     public static Identifier id(String path) {
         return new Identifier(MOD_ID, path);
     }
 
-    public static final Identifier SHULKER_CHEST = new Identifier(MOD_ID, "shulker_chest");
-    public static final String SHULKER_CHEST_TRANSLATION_KEY = Util.createTranslationKey("shulker_chest", SHULKER_CHEST);
-
-
     @Override
     public void onInitialize() {
-
-        ContainerProviderRegistry.INSTANCE.registerFactory(SHULKER_CHEST, (syncId, identifier, player, buf) -> {
-            final World world = player.world;
-            final BlockPos pos = buf.readBlockPos();
-            return world.getBlockState(pos).createContainerFactory(player.world, pos).createMenu(syncId, player.inventory, player);
-        });
-
         JinericItems.init();
         JinericBlocks.init();
         JinericItemGroups.init();
         JinericWorldGen.init();
         JinericSounds.init();
+        JinericShulkerChestBlock.init();
+        JinericShulkerChestBlockEntity.init();
     }
 }
