19220ba9607219617a602c65156f0c28f1c955df
Eric <69827559+Jiingy@users.noreply.github.com>
2021-08-21 19:29:06 -0700

    Mass reorganization of JinericItems, and additions of new item groups. (now modded item groups follow vanilla)

--- src/main/java/jingy/jineric/items/JinericItemGroups.java
+++ src/main/java/jingy/jineric/items/JinericItemGroups.java
@@ -4,42 +4,48 @@ import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 
 public class JinericItemGroups {
-	public static final ItemGroup JINERIC_BLOCKS = FabricItemGroupBuilder.build(
-			new Identifier("blocks", "jineric"),
+	public static final ItemGroup BUILDING_BLOCKS = FabricItemGroupBuilder.build(
+			new Identifier("building_blocks", "jineric"),
 			() -> new ItemStack(JinericItems.SILTSTONE));
-	public static final ItemGroup JINERIC_BREWING = FabricItemGroupBuilder.build(
-			new Identifier("brewing", "jineric"),
 
-			() -> new ItemStack(JinericItems.GOLDEN_SWEET_BERRIES));
-	public static final ItemGroup JINERIC_MISCELLANEOUS = FabricItemGroupBuilder.build(
+	public static final ItemGroup DECORATION_BLOCKS = FabricItemGroupBuilder.build(
+			new Identifier("decoration_blocks", "jineric"),
+			() -> new ItemStack(JinericItems.BAMBOO_BLOCK));
+
+	public static final ItemGroup REDSTONE = FabricItemGroupBuilder.build(
+			new Identifier("redstone", "jineric"),
+			() -> new ItemStack(Items.REDSTONE));
+
+	public static final ItemGroup TRANSPORTATION = FabricItemGroupBuilder.build(
+			new Identifier("transportation", "jineric"),
+			() -> new ItemStack(Items.POWERED_RAIL));
+
+	public static final ItemGroup MISCELLANEOUS = FabricItemGroupBuilder.build(
 			new Identifier("miscellaneous", "jineric"),
-
 			() -> new ItemStack(JinericItems.SALT));
 
-	public static Item.Settings jinericBlocks() {
-		return new Item.Settings().group(JINERIC_BLOCKS);
+	public static final ItemGroup FOODS = FabricItemGroupBuilder.build(
+			new Identifier("foods", "jineric"),
+			() -> new ItemStack(Items.APPLE));
 
-	}
+	public static final ItemGroup TOOLS = FabricItemGroupBuilder.build(
+			new Identifier("tools", "jineric"),
+			() -> new ItemStack(Items.IRON_AXE));
 
-	public static Item.Settings jinericBrewing() {
-		return new Item.Settings().group(JINERIC_BREWING);
+	public static final ItemGroup COMBAT = FabricItemGroupBuilder.build(
+			new Identifier("combat", "jineric"),
+			() -> new ItemStack(Items.GOLDEN_SWORD));
 
-	}
-
-	public static Item.Settings jinericMiscellaneous() {
-		return new Item.Settings().group(JINERIC_MISCELLANEOUS);
-
-	/*}
-	public static final ItemGroup JINERIC_BREWING = FabricItemGroupBuilder.build(
+	public static final ItemGroup BREWING = FabricItemGroupBuilder.build(
 			new Identifier("brewing", "jineric"),
+			() -> new ItemStack(Items.POTION));
 
-			() -> new ItemStack(JinericItems.GOLDEN_SWEET_BERRIES));
-	public static Item.Settings jinericBrewing() {
-		return new Item.Settings().group(JINERIC_BREWING);
-*/
+	public static Item.Settings jinericBlocks() {
+		return new Item.Settings().group(BUILDING_BLOCKS);
 
 	}
 
