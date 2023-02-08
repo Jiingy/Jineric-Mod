8ff25fdb90ce5272b061afe23b8e053b1a1f8643
Jingy <eric.sharp224@gmail.com>
2023-02-08 15:35:49 -0600

    Fixed issues with FullGrassBlock - Edible by sheep - Block model cleaner & randomly rotates - Allows for GrassBlocks to survive underneath them, but not spread
    Signed-off-by: Jingy <eric.sharp224@gmail.com>

--- src/main/java/jingy/jineric/mixin/EatGrassGoalMixin.java
+++ src/main/java/jingy/jineric/mixin/EatGrassGoalMixin.java
@@ -1,22 +1,17 @@
 package jingy.jineric.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import jingy.jineric.block.JinericBlocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.entity.ai.goal.EatGrassGoal;
 import net.minecraft.entity.ai.goal.Goal;
-import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.predicate.block.BlockStatePredicate;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.GameRules;
-import net.minecraft.world.World;
-import net.minecraft.world.WorldEvents;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 import java.util.function.Predicate;
 
@@ -24,60 +19,35 @@ import java.util.function.Predicate;
 @Mixin(EatGrassGoal.class)
 public abstract class EatGrassGoalMixin extends Goal {
 
-   @Shadow @Final private World world;
-   @Shadow @Final private MobEntity mob;
    @Mutable
-   @Shadow
-   private int timer;
+   @Shadow @Final
+   public static Predicate<BlockState> GRASS_PREDICATE;
 
-   @Shadow @Final private static final Predicate<BlockState> GRASS_PREDICATE = BlockStatePredicate.forBlock(Blocks.GRASS).or(BlockStatePredicate.forBlock(JinericBlocks.FULL_GRASS_BLOCK));
-
-   @Inject(method = "canStart", at = @At(value = "HEAD", target = "net/minecraft/block/BlockState.isOf(Lnet/minecraft/block/Block;)Z"), cancellable = true)
-   public void jineric$canStart(CallbackInfoReturnable<Boolean> cir) {
-      if (this.mob.getRandom().nextInt(this.mob.isBaby() ? 50 : 1000) != 0) {
-         cir.setReturnValue(false);
-      } else {
-         BlockPos blockPos = this.mob.getBlockPos();
-         if (GRASS_PREDICATE.test(this.world.getBlockState(blockPos))) {
-            cir.setReturnValue(true);
-         } else {
-            cir.setReturnValue(this.world.getBlockState(blockPos.down()).isOf(Blocks.GRASS_BLOCK) || this.world.getBlockState(blockPos.down()).isOf(JinericBlocks.FULL_GRASS_BLOCK));
-         }
-      }
-      cir.cancel();
+   static {
+      GRASS_PREDICATE = GRASS_PREDICATE.or(BlockStatePredicate.forBlock(JinericBlocks.FULL_GRASS_BLOCK));
    }
 
-   @Inject(method = "tick", at = @At(value = "HEAD"), cancellable = true)
-   public void jineric$tick(CallbackInfo ci) {
-      this.timer = Math.max(0, this.timer - 1);
-      if (this.timer == this.getTickCount(4)) {
-         BlockPos blockPos = this.mob.getBlockPos();
-         if (GRASS_PREDICATE.test(this.world.getBlockState(blockPos))) {
-            if (this.world.getGameRules().getBoolean(GameRules.DO_MOB_GRIEFING)) {
-               this.world.breakBlock(blockPos, false);
-            }
+   @WrapOperation(
+           method = "canStart",
+           at = @At(value = "INVOKE", target = "net/minecraft/block/BlockState.isOf(Lnet/minecraft/block/Block;)Z")
+   )
+   private boolean jineric$canStartFullGrassBlockCheck(BlockState instance, Block block, Operation<Boolean> original) {
+      return original.call(instance, block) || instance.isOf(JinericBlocks.FULL_GRASS_BLOCK);
+   }
 
-            this.mob.onEatingGrass();
-         } else {
-            BlockPos blockPos2 = blockPos.down();
-            if (this.world.getBlockState(blockPos2).isOf(Blocks.GRASS_BLOCK)) {
-               if (this.world.getGameRules().getBoolean(GameRules.DO_MOB_GRIEFING)) {
-                  this.world.syncWorldEvent(WorldEvents.BLOCK_BROKEN, blockPos2, Block.getRawIdFromState(Blocks.GRASS_BLOCK.getDefaultState()));
-                  this.world.setBlockState(blockPos2, Blocks.DIRT.getDefaultState(), Block.NOTIFY_LISTENERS);
-               }
+   @WrapOperation(
+           method = "tick",
+           at = @At(value = "INVOKE", target = "net/minecraft/block/BlockState.isOf(Lnet/minecraft/block/Block;)Z")
+   )
+   private boolean jineric$tickFullGrassBlockCheck(BlockState instance, Block block, Operation<Boolean> original) {
+      return original.call(instance, block) || instance.isOf(JinericBlocks.FULL_GRASS_BLOCK);
+   }
 
-               this.mob.onEatingGrass();
-            }
-            if (this.world.getBlockState(blockPos2).isOf(JinericBlocks.FULL_GRASS_BLOCK)) {
-               if (this.world.getGameRules().getBoolean(GameRules.DO_MOB_GRIEFING)) {
-                  this.world.syncWorldEvent(WorldEvents.BLOCK_BROKEN, blockPos2, Block.getRawIdFromState(JinericBlocks.FULL_GRASS_BLOCK.getDefaultState()));
-                  this.world.setBlockState(blockPos2, Blocks.DIRT.getDefaultState(), Block.NOTIFY_LISTENERS);
-               }
-
-               this.mob.onEatingGrass();
-            }
-         }
-      }
-      ci.cancel();
+   @ModifyArg(
+           method = "tick",
+           at = @At(value = "INVOKE", target = "Lnet/minecraft/block/Block;getRawIdFromState(Lnet/minecraft/block/BlockState;)I")
+   )
+   private BlockState jineric$FullGrassRawIdFromState(@Nullable BlockState state) {
+      return JinericBlocks.FULL_GRASS_BLOCK.getDefaultState();
    }
 }
---
+++ src/main/java/jingy/jineric/mixin/SpreadableBlockMixin.java
@@ -0,0 +1,36 @@
+package jingy.jineric.mixin;
+
+import jingy.jineric.block.JinericBlocks;
+import jingy.jineric.tag.JinericBlockTags;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.SpreadableBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.WorldView;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(SpreadableBlock.class)
+public abstract class SpreadableBlockMixin {
+
+   @Inject(method = "canSurvive", at = @At(value = "HEAD"), cancellable = true)
+   private static void jineric$canSurviveIfFullGrassBlock(BlockState state, WorldView world, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
+      BlockPos blockPos = pos.up();
+      BlockState blockState = world.getBlockState(blockPos);
+      if (blockState.isIn(JinericBlockTags.FULL_GRASSY_BLOCKS)) {
+         cir.setReturnValue(true);
+      }
+   }
+
+   @Inject(
+           method = "canSpread",
+           at = @At(value = "RETURN", target = "net/minecraft/block/SpreadableBlock.canSurvive(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/WorldView;Lnet/minecraft/util/math/BlockPos;)Z"),
+           cancellable = true)
+   private static void jineric$cannotSpreadIfFullGrassBlock(BlockState state, WorldView world, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
+      BlockPos posAbove = pos.up();
+      if (world.getBlockState(posAbove).isOf(JinericBlocks.FULL_GRASS_BLOCK)) {
+         cir.setReturnValue(false);
+      }
+   }
+}
