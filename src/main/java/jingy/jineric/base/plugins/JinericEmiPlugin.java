57d0a8cd76ccd9249af77f3ae4f6e52499eb34d3
Jingy <69827559+Jiingy@users.noreply.github.com>
2023-04-13 01:27:31 -0500

    Added EMI support
    Signed-off-by: Jingy <69827559+Jiingy@users.noreply.github.com>

---
+++ src/main/java/jingy/jineric/base/plugins/JinericEmiPlugin.java
@@ -0,0 +1,11 @@
+package jingy.jineric.base.plugins;
+
+import dev.emi.emi.api.EmiPlugin;
+import dev.emi.emi.api.EmiRegistry;
+
+public class JinericEmiPlugin implements EmiPlugin {
+   @Override
+   public void register(EmiRegistry registry) {
+
+   }
+}
