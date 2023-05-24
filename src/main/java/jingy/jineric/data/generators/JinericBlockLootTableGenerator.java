c3f9e0044da6692df5334dc62e1c4457808e33ef
Jingy <69827559+Jiingy@users.noreply.github.com>
2023-05-23 23:46:15 -0500

    Datagen for chest loot tables / added smithing for netherite horse armor
    Signed-off-by: Jingy <69827559+Jiingy@users.noreply.github.com>

--- src/main/java/jingy/jineric/data/generators/JinericBlockLootTableGenerator.java
+++ src/main/java/jingy/jineric/data/generators/JinericBlockLootTableGenerator.java
@@ -1,5 +1,6 @@
 package jingy.jineric.data.generators;
 
+import jingy.jineric.block.JinericBlocks;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
 import net.minecraft.block.Block;
@@ -11,6 +12,28 @@ public class JinericBlockLootTableGenerator extends FabricBlockLootTableProvider
 
    @Override
    public void generate() {
+      nameableContainersGen();
+   }
+
+   public void nameableContainersGen() {
+      //Chests
+      addDrop(JinericBlocks.SPRUCE_CHEST, this::nameableContainerDrops);
+      addDrop(JinericBlocks.BIRCH_CHEST, this::nameableContainerDrops);
+      addDrop(JinericBlocks.JUNGLE_CHEST, this::nameableContainerDrops);
+      addDrop(JinericBlocks.ACACIA_CHEST, this::nameableContainerDrops);
+      addDrop(JinericBlocks.DARK_OAK_CHEST, this::nameableContainerDrops);
+      addDrop(JinericBlocks.MANGROVE_CHEST, this::nameableContainerDrops);
+      addDrop(JinericBlocks.CRIMSON_CHEST, this::nameableContainerDrops);
+      addDrop(JinericBlocks.WARPED_CHEST, this::nameableContainerDrops);
+      //Trapped Chests
+      addDrop(JinericBlocks.TRAPPED_SPRUCE_CHEST, this::nameableContainerDrops);
+      addDrop(JinericBlocks.TRAPPED_BIRCH_CHEST, this::nameableContainerDrops);
+      addDrop(JinericBlocks.TRAPPED_JUNGLE_CHEST, this::nameableContainerDrops);
+      addDrop(JinericBlocks.TRAPPED_ACACIA_CHEST, this::nameableContainerDrops);
+      addDrop(JinericBlocks.TRAPPED_DARK_OAK_CHEST, this::nameableContainerDrops);
+      addDrop(JinericBlocks.TRAPPED_MANGROVE_CHEST, this::nameableContainerDrops);
+      addDrop(JinericBlocks.TRAPPED_CRIMSON_CHEST, this::nameableContainerDrops);
+      addDrop(JinericBlocks.TRAPPED_WARPED_CHEST, this::nameableContainerDrops);
    }
 
    public void addBlockSetDrops(Block block, Block stairBlock, Block slabBlock, Block wallBlock) {
