50552fc79356608859bc4647c93f3e6077c7f146
Jingy <69827559+Jiingy@users.noreply.github.com>
2023-06-09 03:12:06 -0500

    Added CookingRecipeJsonBuilderAccess.java
    Signed-off-by: Jingy <69827559+Jiingy@users.noreply.github.com>

---
+++ src/main/java/jingy/jineric/mixin/access/CookingRecipeJsonBuilderAccess.java
@@ -0,0 +1,28 @@
+package jingy.jineric.mixin.access;
+
+import net.minecraft.data.server.recipe.CookingRecipeJsonBuilder;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.recipe.AbstractCookingRecipe;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.recipe.RecipeSerializer;
+import net.minecraft.recipe.book.CookingRecipeCategory;
+import net.minecraft.recipe.book.RecipeCategory;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(CookingRecipeJsonBuilder.class)
+public interface CookingRecipeJsonBuilderAccess {
+
+   @Invoker("<init>")
+   static CookingRecipeJsonBuilder invokeInit(
+           RecipeCategory category,
+           CookingRecipeCategory cookingCategory,
+           ItemConvertible output,
+           Ingredient input,
+           float experience,
+           int cookingTime,
+           RecipeSerializer<? extends AbstractCookingRecipe> serializer
+   ) {
+      throw new IllegalStateException("Mixin not applied!");
+   }
+}
