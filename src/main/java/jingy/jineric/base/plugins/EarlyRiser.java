3489d2f1f4b097743af010bda00e78ac5174eed1
Jingy <69827559+Jiingy@users.noreply.github.com>
2023-06-10 17:48:06 -0500

    Fixed issues: - corruption of player data - recipe book for refinery not checking if on client
    Signed-off-by: Jingy <69827559+Jiingy@users.noreply.github.com>

--- src/main/java/jingy/jineric/base/plugins/EarlyRiser.java
+++ src/main/java/jingy/jineric/base/plugins/EarlyRiser.java
@@ -1,6 +1,7 @@
 package jingy.jineric.base.plugins;
 
 import com.chocohead.mm.api.ClassTinkerers;
+import net.fabricmc.api.EnvType;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.MappingResolver;
 import net.minecraft.item.ItemStack;
@@ -10,15 +11,17 @@ public class EarlyRiser implements Runnable {
 
    @Override
    public void run() {
+      boolean isClient = FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT;
       MappingResolver remapper = FabricLoader.getInstance().getMappingResolver();
       String recipeBookCategory = remapper.mapClassName("intermediary", "net.minecraft.class_5421");
       ClassTinkerers.enumBuilder(recipeBookCategory).addEnum("JINERIC_REFINERY").build();
 
-      String recipeBookGroup = remapper.mapClassName("intermediary", "net.minecraft.class_314");
-      String itemStack = "[L" + remapper.mapClassName("intermediary", "net.minecraft.class_1799") + ";";
-      ClassTinkerers.enumBuilder(recipeBookGroup, itemStack).addEnum("JINERIC_REFINERY_SEARCH", () -> new Object[] {new ItemStack[]{new ItemStack(Items.COMPASS)}}).build();
-      ClassTinkerers.enumBuilder(recipeBookGroup, itemStack).addEnum("JINERIC_REFINERY_BUILDING", () -> new Object[] {new ItemStack[]{new ItemStack(Items.STONE)}}).build();
-      ClassTinkerers.enumBuilder(recipeBookGroup, itemStack).addEnum("JINERIC_REFINERY_MISC", () -> new Object[] {new ItemStack[]{new ItemStack(Items.CYAN_GLAZED_TERRACOTTA)}}).build();
-
+      if (isClient) {
+         String recipeBookGroup = remapper.mapClassName("intermediary", "net.minecraft.class_314");
+         String itemStack = "[L" + remapper.mapClassName("intermediary", "net.minecraft.class_1799") + ";";
+         ClassTinkerers.enumBuilder(recipeBookGroup, itemStack).addEnum("JINERIC_REFINERY_SEARCH", () -> new Object[] {new ItemStack[]{new ItemStack(Items.COMPASS)}}).build();
+         ClassTinkerers.enumBuilder(recipeBookGroup, itemStack).addEnum("JINERIC_REFINERY_BUILDING", () -> new Object[] {new ItemStack[]{new ItemStack(Items.STONE)}}).build();
+         ClassTinkerers.enumBuilder(recipeBookGroup, itemStack).addEnum("JINERIC_REFINERY_MISC", () -> new Object[] {new ItemStack[]{new ItemStack(Items.CYAN_GLAZED_TERRACOTTA)}}).build();
+      }
    }
 }
--- src/main/java/jingy/jineric/mixin/RecipeBookGroupMixin.java
+++ src/main/java/jingy/jineric/mixin/RecipeBookGroupMixin.java
@@ -5,11 +5,12 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import jingy.jineric.recipe.JinericRecipeBookGroup;
 import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.recipebook.RecipeBookGroup;
 import net.minecraft.recipe.book.RecipeBookCategory;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -20,9 +21,8 @@ import java.util.Map;
 
 import static net.minecraft.client.recipebook.RecipeBookGroup.*;
 
-@Environment(value= EnvType.CLIENT)
 @Mixin(RecipeBookGroup.class)
-public class RecipeBookGroupMixin {
+public abstract class RecipeBookGroupMixin {
 
    private static final RecipeBookGroup JINERIC_REFINERY_SEARCH = ClassTinkerers.getEnum(RecipeBookGroup.class, "JINERIC_REFINERY_SEARCH");
    private static final RecipeBookGroup JINERIC_REFINERY_BUILDING = ClassTinkerers.getEnum(RecipeBookGroup.class, "JINERIC_REFINERY_BUILDING");
@@ -37,23 +37,30 @@ public class RecipeBookGroupMixin {
    @Final
    @Shadow private static RecipeBookGroup SMOKER_SEARCH;
    private static final RecipeBookCategory JINERIC_REFINERY = ClassTinkerers.getEnum(RecipeBookCategory.class, "JINERIC_REFINERY");
-   private static final Map<RecipeBookGroup, List<RecipeBookGroup>> SEARCH_MAP = ImmutableMap.of(
-           CRAFTING_SEARCH,
-           ImmutableList.of(CRAFTING_EQUIPMENT, CRAFTING_BUILDING_BLOCKS, CRAFTING_MISC, CRAFTING_REDSTONE),
-           FURNACE_SEARCH,
-           ImmutableList.of(FURNACE_FOOD, FURNACE_BLOCKS, FURNACE_MISC),
-           BLAST_FURNACE_SEARCH,
-           ImmutableList.of(BLAST_FURNACE_BLOCKS, BLAST_FURNACE_MISC),
-           SMOKER_SEARCH,
-           ImmutableList.of(SMOKER_FOOD),
-           JINERIC_REFINERY_SEARCH,
-           ImmutableList.of(JINERIC_REFINERY_BUILDING, JINERIC_REFINERY_MISC)
-   );
+
+   @Shadow
+   @Mutable
+   @Final
+   private static Map<RecipeBookGroup, List<RecipeBookGroup>> SEARCH_MAP;
+
+   static {
+      if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
+         SEARCH_MAP = ImmutableMap.of(
+                 CRAFTING_SEARCH, ImmutableList.of(CRAFTING_EQUIPMENT, CRAFTING_BUILDING_BLOCKS, CRAFTING_MISC, CRAFTING_REDSTONE),
+                 FURNACE_SEARCH, ImmutableList.of(FURNACE_FOOD, FURNACE_BLOCKS, FURNACE_MISC),
+                 BLAST_FURNACE_SEARCH, ImmutableList.of(BLAST_FURNACE_BLOCKS, BLAST_FURNACE_MISC),
+                 SMOKER_SEARCH, ImmutableList.of(SMOKER_FOOD),
+                 JINERIC_REFINERY_SEARCH, ImmutableList.of(JINERIC_REFINERY_BUILDING, JINERIC_REFINERY_MISC)
+         );
+      }
+   }
 
    @Inject(method = "getGroups", at = @At("HEAD"), cancellable = true)
    private static void jineric$checkForRefinery(RecipeBookCategory category, CallbackInfoReturnable<List<RecipeBookGroup>> cir) {
-      if (category == JINERIC_REFINERY) {
-         cir.setReturnValue(JinericRecipeBookGroup.JINERIC_REFINERY);
+      if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
+         if (category == JINERIC_REFINERY) {
+            cir.setReturnValue(JinericRecipeBookGroup.JINERIC_REFINERY);
+         }
       }
    }
 }
