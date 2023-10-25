99d2804802093cc217fda267790ec10f68f3e913
Jingy <69827559+Jiingy@users.noreply.github.com>
2023-10-25 00:01:27 -0500

    Added refinery recipe book icons
    Signed-off-by: Jingy <69827559+Jiingy@users.noreply.github.com>

--- src/main/java/jingy/jineric/screen/RefineryRecipeBookScreen.java
+++ src/main/java/jingy/jineric/screen/RefineryRecipeBookScreen.java
@@ -4,16 +4,21 @@ import jingy.jineric.base.JinericMain;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.block.entity.AbstractFurnaceBlockEntity;
+import net.minecraft.client.gui.screen.ButtonTextures;
 import net.minecraft.client.gui.screen.recipebook.AbstractFurnaceRecipeBookScreen;
 import net.minecraft.item.Item;
 import net.minecraft.text.Text;
-import net.minecraft.util.Identifier;
 
 import java.util.Set;
 
 @Environment(EnvType.CLIENT)
 public class RefineryRecipeBookScreen extends AbstractFurnaceRecipeBookScreen {
-   protected static final Identifier REFINERY_TEXTURE = JinericMain.id("textures/gui/container/refinery_recipe_book.png");
+   private static final ButtonTextures REFINERY_TEXTURES = new ButtonTextures(
+           JinericMain.id("recipe_book/refinery_filter_enabled"),
+           JinericMain.id("recipe_book/refinery_filter_disabled"),
+           JinericMain.id("recipe_book/refinery_filter_enabled_highlighted"),
+           JinericMain.id("recipe_book/refinery_filter_disabled_highlighted")
+   );
    private static final Text TOGGLE_REFINABLE_RECIPES_TEXT = Text.translatable("gui.recipebook.toggleRecipes.refinable");
 
    @Override
@@ -22,7 +27,7 @@ public class RefineryRecipeBookScreen extends AbstractFurnaceRecipeBookScreen {
    }
 
    protected void setBookButtonTexture() {
-      this.toggleCraftableButton.setTextureUV(152, 182, 28, 18, REFINERY_TEXTURE);
+      this.toggleCraftableButton.setTextures(REFINERY_TEXTURES);
    }
 
    @Override
