e5cd363075c9993711d68b7464ece765b920a829
Jingy <69827559+Jiingy@users.noreply.github.com>
2023-11-01 14:33:55 -0500

    Cleaned up jineric recipe generation for future usage split the generators, and the generated content into different classes for the sake of clarity.
    Signed-off-by: Jingy <69827559+Jiingy@users.noreply.github.com>

--- src/main/java/jingy/jineric/data/JinericDataGeneration.java
+++ src/main/java/jingy/jineric/data/JinericDataGeneration.java
@@ -2,8 +2,8 @@ package jingy.jineric.data;
 
 import jingy.jineric.base.JinericMain;
 import jingy.jineric.data.generators.JinericBlockLootTableGenerator;
+import jingy.jineric.data.generators.JinericGeneratedRecipes;
 import jingy.jineric.data.generators.JinericModelGenerator;
-import jingy.jineric.data.generators.JinericRecipeGenerator;
 import jingy.jineric.data.generators.world.JinericWorldGenerator;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
@@ -15,7 +15,7 @@ public class JinericDataGeneration implements DataGeneratorEntrypoint {
    @Override
    public void onInitializeDataGenerator(FabricDataGenerator generator) {
       FabricDataGenerator.Pack fabricDataGenPack = generator.createPack();
-      fabricDataGenPack.addProvider(JinericRecipeGenerator::new);
+      fabricDataGenPack.addProvider(JinericGeneratedRecipes::new);
       fabricDataGenPack.addProvider(JinericModelGenerator::new);
       fabricDataGenPack.addProvider(JinericBlockLootTableGenerator::new);
       fabricDataGenPack.addProvider(JinericWorldGenerator::new);
