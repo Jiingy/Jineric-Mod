57a3d488bd74f0eaf48e918dd0cb411fa6919d7c
Jingy <69827559+Jiingy@users.noreply.github.com>
2023-11-01 12:56:06 -0500

    Added tag for jineric trapped chests, updated other tags
    Signed-off-by: Jingy <69827559+Jiingy@users.noreply.github.com>

--- src/main/java/jingy/jineric/tag/JinericBlockTags.java
+++ src/main/java/jingy/jineric/tag/JinericBlockTags.java
@@ -4,20 +4,20 @@ import jingy.jineric.base.JinericMain;
 import net.minecraft.block.Block;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.tag.TagKey;
-import net.minecraft.util.Identifier;
 
 public class JinericBlockTags {
-   public static final TagKey<Block> SPECIALTY_WALLS = register("specialty_walls");
-   public static final TagKey<Block> IS_SLIPPERY = register("is_slippery");
-   public static final TagKey<Block> FULL_GRASS_REPLACEABLE = register("full_grass_block_replaceable");
-   public static final TagKey<Block> WOODEN_BOOKSHELVES = register("wooden_bookshelves");
-   public static final TagKey<Block> CHESTS = register("chests");
-   public static final TagKey<Block> WOODEN_CHESTS = register("wooden_chests");
-   public static final TagKey<Block> WOODEN_LADDERS = register("wooden_ladders");
-   public static final TagKey<Block> FULL_GRASSY_BLOCKS = register("full_grassy_blocks");
+   public static final TagKey<Block> SPECIALTY_WALLS = registerBlockTag("specialty_walls");
+   public static final TagKey<Block> IS_SLIPPERY = registerBlockTag("is_slippery");
+   public static final TagKey<Block> FULL_GRASS_REPLACEABLE = registerBlockTag("full_grass_block_replaceable");
+   public static final TagKey<Block> WOODEN_BOOKSHELVES = registerBlockTag("wooden_bookshelves");
+   public static final TagKey<Block> CHESTS = registerBlockTag("chests");
+   public static final TagKey<Block> WOODEN_CHESTS = registerBlockTag("wooden_chests");
+   public static final TagKey<Block> WOODEN_TRAPPED_CHESTS = registerBlockTag("wooden_trapped_chests");
+   public static final TagKey<Block> WOODEN_LADDERS = registerBlockTag("wooden_ladders");
+   public static final TagKey<Block> FULL_GRASSY_BLOCKS = registerBlockTag("full_grassy_blocks");
 
-   private static TagKey<Block> register(String id) {
-      return TagKey.of(Registries.BLOCK.getKey(), new Identifier(JinericMain.MOD_ID, id));
+   private static TagKey<Block> registerBlockTag(String id) {
+      return TagKey.of(Registries.BLOCK.getKey(), JinericMain.id(id));
    }
 
    private JinericBlockTags() {
