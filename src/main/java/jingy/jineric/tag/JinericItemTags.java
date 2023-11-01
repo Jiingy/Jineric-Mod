29f0d2359b6d29c91bd113cbfd5c141eff9821c7
Jingy <69827559+Jiingy@users.noreply.github.com>
2023-11-01 12:55:45 -0500

    Added missing item jineric item tags
    Signed-off-by: Jingy <69827559+Jiingy@users.noreply.github.com>

--- src/main/java/jingy/jineric/tag/JinericItemTags.java
+++ src/main/java/jingy/jineric/tag/JinericItemTags.java
@@ -1,17 +1,19 @@
 package jingy.jineric.tag;
 
+import jingy.jineric.base.JinericMain;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.tag.TagKey;
-import net.minecraft.util.Identifier;
 
 public class JinericItemTags {
-   public static final TagKey<Item> WOODEN_BOOKSHELVES = register("jineric:wooden_bookshelves");
-   public static final TagKey<Item> WOODEN_LADDERS = register("jineric:wooden_ladders");
-   public static final TagKey<Item> WOODEN_CHESTS = register("jineric:wooden_chests");
+   public static final TagKey<Item> CHESTS = registerItemTag("chests");
+   public static final TagKey<Item> WOODEN_BOOKSHELVES = registerItemTag("wooden_bookshelves");
+   public static final TagKey<Item> WOODEN_CHESTS = registerItemTag("wooden_chests");
+   public static final TagKey<Item> WOODEN_LADDERS = registerItemTag("wooden_ladders");
+   public static final TagKey<Item> WOODEN_TRAPPED_CHESTS = registerItemTag("wooden_trapped_chests");
 
-   private static TagKey<Item> register(String id) {
-      return TagKey.of(Registries.ITEM.getKey(), new Identifier(id));
+   private static TagKey<Item> registerItemTag(String id) {
+      return TagKey.of(Registries.ITEM.getKey(), JinericMain.id(id));
    }
 
    private JinericItemTags() {
