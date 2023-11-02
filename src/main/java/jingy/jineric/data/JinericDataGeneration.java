d0d4efab4d3dcd599d799230c571b695c99436e8
Jingy <69827559+Jiingy@users.noreply.github.com>
2023-11-02 16:24:20 -0500

    Undid one change from previous commit
    Signed-off-by: Jingy <69827559+Jiingy@users.noreply.github.com>

--- src/main/java/jingy/jineric/data/JinericDataGeneration.java
+++ src/main/java/jingy/jineric/data/JinericDataGeneration.java
@@ -2,9 +2,9 @@ package jingy.jineric.data;
 
 import jingy.jineric.base.JinericMain;
 import jingy.jineric.data.generators.JinericBlockLootTableGenerator;
+import jingy.jineric.data.generators.JinericBlockTagGenerator;
 import jingy.jineric.data.generators.JinericGeneratedRecipes;
 import jingy.jineric.data.generators.JinericModelGenerator;
-import jingy.jineric.data.generators.JinericTagGenerator;
 import jingy.jineric.data.generators.world.JinericWorldGenerator;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
@@ -17,10 +17,10 @@ public class JinericDataGeneration implements DataGeneratorEntrypoint {
    public void onInitializeDataGenerator(FabricDataGenerator generator) {
       FabricDataGenerator.Pack fabricDataGenPack = generator.createPack();
       fabricDataGenPack.addProvider(JinericBlockLootTableGenerator::new);
+      fabricDataGenPack.addProvider(JinericBlockTagGenerator::new);
       fabricDataGenPack.addProvider(JinericModelGenerator::new);
       fabricDataGenPack.addProvider(JinericWorldGenerator::new);
       fabricDataGenPack.addProvider(JinericGeneratedRecipes::new);
-      fabricDataGenPack.addProvider(JinericTagGenerator::new);
    }
 
    @Override
