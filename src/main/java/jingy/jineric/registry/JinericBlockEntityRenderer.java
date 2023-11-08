dd22a80c7c5bd7cc3343759606a1d7e15c2580ea
Jingy <69827559+Jiingy@users.noreply.github.com>
2023-11-08 00:32:39 -0600

    Added block entity renderers for signs
    Signed-off-by: Jingy <69827559+Jiingy@users.noreply.github.com>

--- src/main/java/jingy/jineric/registry/JinericBlockEntityRenderer.java
+++ src/main/java/jingy/jineric/registry/JinericBlockEntityRenderer.java
@@ -3,6 +3,8 @@ package jingy.jineric.registry;
 import jingy.jineric.client.render.blockentity.RedstoneCampfireBlockEntityRenderer;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
 import net.minecraft.client.render.block.entity.ChestBlockEntityRenderer;
+import net.minecraft.client.render.block.entity.HangingSignBlockEntityRenderer;
+import net.minecraft.client.render.block.entity.SignBlockEntityRenderer;
 
 public class JinericBlockEntityRenderer {
 
@@ -10,6 +12,8 @@ public class JinericBlockEntityRenderer {
       BlockEntityRendererFactories.register(JinericBlockEntityType.REDSTONE_CAMPFIRE, RedstoneCampfireBlockEntityRenderer::new);
       BlockEntityRendererFactories.register(JinericBlockEntityType.JINERIC_CHEST, ChestBlockEntityRenderer::new);
       BlockEntityRendererFactories.register(JinericBlockEntityType.JINERIC_TRAPPED_CHEST, ChestBlockEntityRenderer::new);
+      BlockEntityRendererFactories.register(JinericBlockEntityType.JINERIC_SIGN, SignBlockEntityRenderer::new);
+      BlockEntityRendererFactories.register(JinericBlockEntityType.JINERIC_HANGING_SIGN, HangingSignBlockEntityRenderer::new);
       BlockEntityRendererFactories.register(JinericBlockEntityType.REDSTONE_CAMPFIRE, RedstoneCampfireBlockEntityRenderer::new);
    }
 }
