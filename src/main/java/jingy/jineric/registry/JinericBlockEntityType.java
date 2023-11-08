8d9d6cb5a56b44a2c8212f5fc2bc8c8eb9426fb1
Jingy <69827559+Jiingy@users.noreply.github.com>
2023-11-08 00:32:51 -0600

    Added block entity types for signs
    Signed-off-by: Jingy <69827559+Jiingy@users.noreply.github.com>

--- src/main/java/jingy/jineric/registry/JinericBlockEntityType.java
+++ src/main/java/jingy/jineric/registry/JinericBlockEntityType.java
@@ -2,10 +2,7 @@ package jingy.jineric.registry;
 
 import jingy.jineric.base.JinericMain;
 import jingy.jineric.block.JinericBlocks;
-import jingy.jineric.block.entity.JinericChestBlockEntity;
-import jingy.jineric.block.entity.JinericTrappedChestBlockEntity;
-import jingy.jineric.block.entity.RedstoneCampfireBlockEntity;
-import jingy.jineric.block.entity.RefineryBlockEntity;
+import jingy.jineric.block.entity.*;
 import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.registry.Registries;
@@ -13,7 +10,6 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class JinericBlockEntityType {
-
    public static final BlockEntityType<JinericChestBlockEntity> JINERIC_CHEST =
            FabricBlockEntityTypeBuilder.create(
                    JinericChestBlockEntity::new,
@@ -28,6 +24,7 @@ public class JinericBlockEntityType {
                    JinericBlocks.CRIMSON_CHEST,
                    JinericBlocks.WARPED_CHEST
            ).build();
+
    public static final BlockEntityType<JinericTrappedChestBlockEntity> JINERIC_TRAPPED_CHEST =
            FabricBlockEntityTypeBuilder.create(
                    JinericTrappedChestBlockEntity::new,
@@ -43,6 +40,20 @@ public class JinericBlockEntityType {
                    JinericBlocks.TRAPPED_WARPED_CHEST
            ).build();
 
+   public static final BlockEntityType<JinericSignBlockEntity> JINERIC_SIGN =
+           FabricBlockEntityTypeBuilder.create(
+                   JinericSignBlockEntity::new,
+                   JinericBlocks.PETRIFIED_OAK_SIGN,
+                   JinericBlocks.PETRIFIED_OAK_WALL_SIGN
+           ).build();
+
+   public static final BlockEntityType<JinericHangingSignBlockEntity> JINERIC_HANGING_SIGN =
+           FabricBlockEntityTypeBuilder.create(
+                   JinericHangingSignBlockEntity::new,
+                   JinericBlocks.PETRIFIED_OAK_HANGING_SIGN,
+                   JinericBlocks.PETRIFIED_OAK_WALL_HANGING_SIGN
+           ).build();
+
    public static final BlockEntityType<RefineryBlockEntity> REFINERY =
            FabricBlockEntityTypeBuilder.create(RefineryBlockEntity::new, JinericBlocks.REFINERY).build();
 
@@ -52,6 +63,8 @@ public class JinericBlockEntityType {
    public static void registerBlockEntities() {
       Registry.register(Registries.BLOCK_ENTITY_TYPE, new Identifier(JinericMain.MOD_ID, "jineric_chest"), JINERIC_CHEST);
       Registry.register(Registries.BLOCK_ENTITY_TYPE, new Identifier(JinericMain.MOD_ID, "jineric_trapped_chest"), JINERIC_TRAPPED_CHEST);
+      Registry.register(Registries.BLOCK_ENTITY_TYPE, new Identifier(JinericMain.MOD_ID, "jineric_sign"), JINERIC_SIGN);
+      Registry.register(Registries.BLOCK_ENTITY_TYPE, new Identifier(JinericMain.MOD_ID, "jineric_hanging_sign"), JINERIC_HANGING_SIGN);
 //      Registry.register(Registries.BLOCK_ENTITY_TYPE, new Identifier(JinericMain.MOD_ID, "shulker_chest"), SHULKER_CHEST);
       Registry.register(Registries.BLOCK_ENTITY_TYPE, new Identifier(JinericMain.MOD_ID, "refinery"), REFINERY);
       Registry.register(Registries.BLOCK_ENTITY_TYPE, new Identifier(JinericMain.MOD_ID, "campfire"), REDSTONE_CAMPFIRE);
