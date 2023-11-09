f585084e328a1b70c7b17e35c535f0880768fec5
Jingy <69827559+Jiingy@users.noreply.github.com>
2023-11-08 23:15:27 -0600

    Made additions to block tags 1. Added leaves to vanilla 'leaves' tag 2. Added tag for log blocks
    Signed-off-by: Jingy <69827559+Jiingy@users.noreply.github.com>

--- src/main/java/jingy/jineric/data/generators/JinericBlockTagGenerator.java
+++ src/main/java/jingy/jineric/data/generators/JinericBlockTagGenerator.java
@@ -3,6 +3,7 @@ package jingy.jineric.data.generators;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.BlockTags;
 
 import java.util.concurrent.CompletableFuture;
 
