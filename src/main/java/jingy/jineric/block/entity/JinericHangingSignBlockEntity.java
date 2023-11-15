9aee70f27bd442f485ca167fb85e8f58c381c797
Jingy <69827559+Jiingy@users.noreply.github.com>
2023-11-14 23:23:42 -0600

    Fixed petrified oak hanging sign not displaying proper screen texture
    Signed-off-by: Jingy <69827559+Jiingy@users.noreply.github.com>

--- src/main/java/jingy/jineric/block/entity/JinericHangingSignBlockEntity.java
+++ src/main/java/jingy/jineric/block/entity/JinericHangingSignBlockEntity.java
@@ -3,10 +3,10 @@ package jingy.jineric.block.entity;
 import jingy.jineric.registry.JinericBlockEntityType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntityType;
-import net.minecraft.block.entity.SignBlockEntity;
+import net.minecraft.block.entity.HangingSignBlockEntity;
 import net.minecraft.util.math.BlockPos;
 
-public class JinericHangingSignBlockEntity extends SignBlockEntity {
+public class JinericHangingSignBlockEntity extends HangingSignBlockEntity {
    public JinericHangingSignBlockEntity(BlockPos pos, BlockState state) {
       super(pos, state);
    }
