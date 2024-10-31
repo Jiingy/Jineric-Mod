ddb139e6664fbe9af5475419439dd51c45828e9e
Eric <jingysgaming@gmail.com>
2024-10-31 01:42:49 -0500

    prev commit

--- src/main/java/jingy/jineric/block/JinericCauldronBehaviors.java
+++ src/main/java/jingy/jineric/block/JinericCauldronBehaviors.java
@@ -5,7 +5,5 @@ import net.minecraft.item.Items;
 
 public class JinericCauldronBehaviors {
    public static void registerCauldronBehaviors() {
-      //TODO DYABLE BUNDLES ADDED VANILLA 1.21.3
-//      CauldronBehavior.WATER_CAULDRON_BEHAVIOR.map().put(Items.BUNDLE, CauldronBehavior.CLEAN_DYEABLE_ITEM);
    }
 }
