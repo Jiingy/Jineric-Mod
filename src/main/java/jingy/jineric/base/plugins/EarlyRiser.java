2202d42d3a56e6df9d9055b878f7359b8441d3b5
Jingy <69827559+Jiingy@users.noreply.github.com>
2024-11-19 20:39:33 -0600

    Fixed issues with refinery recipe book
    Signed-off-by: Jingy <69827559+Jiingy@users.noreply.github.com>

--- src/main/java/jingy/jineric/base/plugins/EarlyRiser.java
+++ src/main/java/jingy/jineric/base/plugins/EarlyRiser.java
@@ -5,8 +5,6 @@ import jingy.jineric.recipe.JinericRecipeBookCategories;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.MappingResolver;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.recipe.book.RecipeBookCategory;
 
 public class EarlyRiser implements Runnable {
@@ -16,17 +14,15 @@ public class EarlyRiser implements Runnable {
       boolean isClient = FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT;
       MappingResolver remapper = FabricLoader.getInstance().getMappingResolver();
       String recipeBookType = remapper.mapClassName("intermediary", "net.minecraft.class_5421");
-      String recipeBookCategory = "[L" + remapper.mapClassName("intermediary", "net.minecraft.class_10355") + ";";
-//    String recipeBookCategory = remapper.mapClassName("intermediary", "net.minecraft.class_10355");
       ClassTinkerers.enumBuilder(recipeBookType).addEnum("JINERIC_REFINERY").build();
 
-      if (isClient) {
-//       String clientRecipeBookType = "[L" + remapper.mapClassName("intermediary", "net.minecraft.class_10331") + ";";
-         String clientRecipeBookType = remapper.mapClassName("intermediary", "net.minecraft.class_10331");
+      String recipeCategory = remapper.mapClassName("intermediary", "net.minecraft.class_7800");
+      ClassTinkerers.enumBuilder(recipeCategory, String.class).addEnum("JINERIC_BLOCK_SETS", "jineric_block_sets").build();
+      ClassTinkerers.enumBuilder(recipeCategory, String.class).addEnum("JINERIC_MISC_BLOCKS", "jineric_misc_blocks").build();
 
-//         String itemStack = "[L" + remapper.mapClassName("intermediary", "net.minecraft.class_1799") + ";";
-//         ClassTinkerers.enumBuilder(recipeBookGroup, itemStack).addEnum("JINERIC_REFINERY_BUILDING", () -> new Object[] {new ItemStack[] {new ItemStack(Items.STONE)}}).build();
-//         ClassTinkerers.enumBuilder(recipeBookGroup, itemStack).addEnum("JINERIC_REFINERY_MISC", () -> new Object[] {new ItemStack[]{new ItemStack(Items.CYAN_GLAZED_TERRACOTTA)}}).build();
+      if (isClient) {
+         String recipeBookCategory = "[L" + remapper.mapClassName("intermediary", "net.minecraft.class_10355") + ";";
+         String clientRecipeBookType = remapper.mapClassName("intermediary", "net.minecraft.class_10331");
 
          ClassTinkerers.enumBuilder(clientRecipeBookType, recipeBookCategory).addEnum(
                  "JINERIC_REFINERY",
