8b1a2e189580d338422e06f628e19cc3190ce629
Jingy <69827559+Jiingy@users.noreply.github.com>
2024-11-26 22:50:38 -0600

    Made changes to vanilla block tags - Added 'LADDERS' - Removed 'FULL_GRASSY_BLOCKS' - Renamed SPECIALTY_WALLS and IS_SLIPPERY
    Signed-off-by: Jingy <69827559+Jiingy@users.noreply.github.com>

--- src/main/java/jingy/jineric/tag/JinericBlockTags.java
+++ src/main/java/jingy/jineric/tag/JinericBlockTags.java
@@ -6,20 +6,19 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.tag.TagKey;
 
 public class JinericBlockTags {
-   public static final TagKey<Block> SPECIALTY_WALLS = registerBlockTag("specialty_walls");
-   public static final TagKey<Block> IS_SLIPPERY = registerBlockTag("is_slippery");
+   public static final TagKey<Block> NOT_PICKAXE_WALL = registerBlockTag("not_pickaxe_wall");
+   public static final TagKey<Block> SLIPPERY = registerBlockTag("slippery");
    public static final TagKey<Block> FULL_GRASS_REPLACEABLE = registerBlockTag("full_grass_block_replaceable");
    public static final TagKey<Block> WOODEN_BOOKSHELVES = registerBlockTag("wooden_bookshelves");
    public static final TagKey<Block> CHESTS = registerBlockTag("chests");
    public static final TagKey<Block> WOODEN_CHESTS = registerBlockTag("wooden_chests");
    public static final TagKey<Block> WOODEN_TRAPPED_CHESTS = registerBlockTag("wooden_trapped_chests");
+   public static final TagKey<Block> LADDERS = registerBlockTag("ladders");
    public static final TagKey<Block> WOODEN_LADDERS = registerBlockTag("wooden_ladders");
-   public static final TagKey<Block> FULL_GRASSY_BLOCKS = registerBlockTag("full_grassy_blocks");
 
    private static TagKey<Block> registerBlockTag(String id) {
       return TagKey.of(Registries.BLOCK.getKey(), JinericMain.ofJineric(id));
    }
 
-   private JinericBlockTags() {
-   }
+   public JinericBlockTags() {}
 }
