405a45f3498b874f88cb78375920ddf46ee30d1a
Jingy <69827559+Jiingy@users.noreply.github.com>
2024-11-27 02:05:38 -0600

    Removed random whitespace from class
    Signed-off-by: Jingy <69827559+Jiingy@users.noreply.github.com>

--- src/main/java/jingy/jineric/mixin/BrewingRecipeRegistryMixin.java
+++ src/main/java/jingy/jineric/mixin/BrewingRecipeRegistryMixin.java
@@ -9,7 +9,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-
 @Mixin(BrewingRecipeRegistry.class)
 public abstract class BrewingRecipeRegistryMixin {
 
