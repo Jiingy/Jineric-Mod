f6b77526d20787d542b7522a287e6bc9de79d26b
Jingy <69827559+Jiingy@users.noreply.github.com>
2025-02-18 16:23:42 -0600

    Fixed being unable to shift click items into refinery input
    Signed-off-by: Jingy <69827559+Jiingy@users.noreply.github.com>

---
+++ src/main/java/jingy/jineric/mixin/fix/FixSoleIngredientGettersMixin.java
@@ -0,0 +1,34 @@
+package jingy.jineric.mixin.fix;
+
+import com.google.common.collect.ImmutableMap;
+import jingy.jineric.recipe.JinericRecipePropertySet;
+import jingy.jineric.recipe.JinericRecipeTypes;
+import net.minecraft.recipe.*;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.resource.SinglePreparationResourceReloader;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.Map;
+
+@Mixin(ServerRecipeManager.class)
+public abstract class FixSoleIngredientGettersMixin extends SinglePreparationResourceReloader<PreparedRecipes> implements RecipeManager {
+    @Mutable @Shadow @Final private static Map<RegistryKey<RecipePropertySet>, ServerRecipeManager.SoleIngredientGetter> SOLE_INGREDIENT_GETTERS;
+
+    @Shadow
+    private static ServerRecipeManager.SoleIngredientGetter cookingIngredientGetter(RecipeType<? extends SingleStackRecipe> expectedType) {
+        return null;
+    }
+
+    static {
+        SOLE_INGREDIENT_GETTERS = ImmutableMap.<RegistryKey<RecipePropertySet>, ServerRecipeManager.SoleIngredientGetter>builder()
+                .putAll(SOLE_INGREDIENT_GETTERS)
+                .put(
+                        JinericRecipePropertySet.REFINERY_INPUT,
+                        cookingIngredientGetter(JinericRecipeTypes.REFINING)
+                )
+                .build();
+    }
+}
