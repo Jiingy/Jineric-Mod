8ff4fd1ffe536bba32fbb5ed3dc3b4678d0201ea
Jingy <69827559+Jiingy@users.noreply.github.com>
2025-02-19 21:44:20 -0600

    Updated WistfulForest.java
    Signed-off-by: Jingy <69827559+Jiingy@users.noreply.github.com>

--- src/main/java/jingy/jineric/world/biome/WistfulForest.java
+++ src/main/java/jingy/jineric/world/biome/WistfulForest.java
@@ -9,7 +9,6 @@ import net.minecraft.world.biome.BiomeKeys;
 import net.minecraft.world.biome.source.util.MultiNoiseUtil;
 import terrablender.api.Region;
 import terrablender.api.RegionType;
-import terrablender.api.VanillaParameterOverlayBuilder;
 
 import java.util.function.Consumer;
 
@@ -20,18 +19,7 @@ public class WistfulForest extends Region {
 
    @Override
    public void addBiomes(Registry<Biome> registry, Consumer<Pair<MultiNoiseUtil.NoiseHypercube, RegistryKey<Biome>>> mapper) {
-      VanillaParameterOverlayBuilder builder = new VanillaParameterOverlayBuilder();
-//
-//      List<MultiNoiseUtil.NoiseHypercube> params = new ParameterUtils.ParameterPointListBuilder()
-//              .temperature(Temperature.NEUTRAL)
-//              .humidity(Humidity.NEUTRAL)
-//              .continentalness(Continentalness.span(Continentalness.MID_INLAND, Continentalness.INLAND))
-//              .erosion(Erosion.EROSION_0, Erosion.EROSION_1)
-//              .depth(Depth.SURFACE)
-//              .weirdness(Weirdness.MID_SLICE_NORMAL_ASCENDING, Weirdness.MID_SLICE_NORMAL_DESCENDING)
-//              .build().forEach(point -> builder.add(point, JinericBiomeKeys.WISTFUL_FOREST));
-//
-//      builder.build();
+//      VanillaParameterOverlayBuilder builder = new VanillaParameterOverlayBuilder();
       this.addBiomeSimilar(mapper, BiomeKeys.FOREST, JinericBiomeKeys.WISTFUL_FOREST);
       this.addBiomeSimilar(mapper, BiomeKeys.FLOWER_FOREST, JinericBiomeKeys.WISTFUL_FLOWER_FOREST);
    }
