86e6fa006a8ccb3862b6e714242cabbca26f5f6a
Jingy <69827559+Jiingy@users.noreply.github.com>
2025-03-05 23:55:01 -0600

    Added rose, roses, and dandelion patches for wistful forest biome
    Signed-off-by: Jingy <69827559+Jiingy@users.noreply.github.com>

--- src/main/java/jingy/jineric/world/gen/feature/JinericVegetationConfiguredFeatures.java
+++ src/main/java/jingy/jineric/world/gen/feature/JinericVegetationConfiguredFeatures.java
@@ -1,11 +1,13 @@
 package jingy.jineric.world.gen.feature;
 
+import jingy.jineric.block.JinericBlocks;
 import net.minecraft.block.Blocks;
 import net.minecraft.registry.Registerable;
 import net.minecraft.registry.RegistryEntryLookup;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.registry.entry.RegistryEntryList;
 import net.minecraft.world.gen.feature.*;
 import net.minecraft.world.gen.stateprovider.BlockStateProvider;
 
@@ -14,6 +16,9 @@ import java.util.List;
 public class JinericVegetationConfiguredFeatures {
 	public static final RegistryKey<ConfiguredFeature<?, ?>> TREES_WISTFUL_FOREST = JinericConfiguredFeatures.of("trees_wistful_forest");
 	public static final RegistryKey<ConfiguredFeature<?, ?>> CARVED_PATCH_PUMPKIN = JinericConfiguredFeatures.of("carved_patch_pumpkin");
+	public static final RegistryKey<ConfiguredFeature<?, ?>> PATCH_ROSE = JinericConfiguredFeatures.of("patch_rose");
+	public static final RegistryKey<ConfiguredFeature<?, ?>> PATCH_ROSES = JinericConfiguredFeatures.of("patch_roses");
+	public static final RegistryKey<ConfiguredFeature<?, ?>> PATCH_BLOSSOMED_DANDELION = JinericConfiguredFeatures.of("patch_dandelion");
 	
 	public static void bootstrap(Registerable<ConfiguredFeature<?, ?>> featureRegisterable) {
 		RegistryEntryLookup<ConfiguredFeature<?, ?>> registryEntryLookup = featureRegisterable.getRegistryLookup(RegistryKeys.CONFIGURED_FEATURE);
@@ -21,7 +26,48 @@ public class JinericVegetationConfiguredFeatures {
 		RegistryEntry<PlacedFeature> petrifiedOakChecked = registryLookupPlaced.getOrThrow(JinericTreePlacedFeatures.PETRIFIED_OAK_CHECKED);
 //		RegistryEntry<PlacedFeature> petrifiedOakBees002 = registryLookupPlaced.getOrThrow(JinericTreePlacedFeatures.PETRIFIED_OAK_BEES_002);
 //		RegistryEntry<PlacedFeature> petrifiedOakBees0002 = registryLookupPlaced.getOrThrow(JinericTreePlacedFeatures.PETRIFIED_OAK_BEES_0002);
-		ConfiguredFeatures.register(featureRegisterable, TREES_WISTFUL_FOREST, Feature.RANDOM_SELECTOR, new RandomFeatureConfig(List.of(new RandomFeatureEntry(petrifiedOakChecked, 0.1F)), petrifiedOakChecked));
-		ConfiguredFeatures.register(featureRegisterable, CARVED_PATCH_PUMPKIN, Feature.RANDOM_PATCH, ConfiguredFeatures.createRandomPatchFeatureConfig(Feature.SIMPLE_BLOCK, new SimpleBlockFeatureConfig(BlockStateProvider.of(Blocks.CARVED_PUMPKIN)), List.of(Blocks.GRASS_BLOCK)));
+		ConfiguredFeatures.register(featureRegisterable,
+				TREES_WISTFUL_FOREST, Feature.RANDOM_SELECTOR,
+				new RandomFeatureConfig(List.of(new RandomFeatureEntry(petrifiedOakChecked, 0.1F)), petrifiedOakChecked)
+		);
+		ConfiguredFeatures.register(featureRegisterable,
+				CARVED_PATCH_PUMPKIN, Feature.RANDOM_PATCH,
+				ConfiguredFeatures.createRandomPatchFeatureConfig(Feature.SIMPLE_BLOCK,
+						new SimpleBlockFeatureConfig(BlockStateProvider.of(Blocks.CARVED_PUMPKIN)),
+						List.of(Blocks.GRASS_BLOCK)
+				)
+		);
+		ConfiguredFeatures.register(
+				featureRegisterable,
+				PATCH_ROSE,
+				Feature.FLOWER,
+				new RandomPatchFeatureConfig(
+						80, 9, 3, PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK, new SimpleBlockFeatureConfig(BlockStateProvider.of(JinericBlocks.ROSE)))
+				)
+		);
+		ConfiguredFeatures.register(featureRegisterable,
+				PATCH_ROSES,
+				Feature.SIMPLE_RANDOM_SELECTOR,
+				new SimpleRandomFeatureConfig(
+						RegistryEntryList.of(
+								PlacedFeatures.createEntry(
+										Feature.RANDOM_PATCH,
+										ConfiguredFeatures.createRandomPatchFeatureConfig(Feature.SIMPLE_BLOCK, new SimpleBlockFeatureConfig(BlockStateProvider.of(JinericBlocks.ROSE)))
+								),
+								PlacedFeatures.createEntry(
+										Feature.RANDOM_PATCH,
+										ConfiguredFeatures.createRandomPatchFeatureConfig(Feature.SIMPLE_BLOCK, new SimpleBlockFeatureConfig(BlockStateProvider.of(Blocks.ROSE_BUSH)))
+								)
+						)
+				)
+		);
+		ConfiguredFeatures.register(
+				featureRegisterable,
+				PATCH_BLOSSOMED_DANDELION,
+				Feature.FLOWER,
+				new RandomPatchFeatureConfig(
+						80, 9, 3, PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK, new SimpleBlockFeatureConfig(BlockStateProvider.of(JinericBlocks.BLOSSOMED_DANDELION)))
+				)
+		);
 	}
 }
--- src/main/java/jingy/jineric/world/gen/feature/JinericVegetationPlacedFeatures.java
+++ src/main/java/jingy/jineric/world/gen/feature/JinericVegetationPlacedFeatures.java
@@ -5,22 +5,32 @@ import net.minecraft.registry.RegistryEntryLookup;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.util.math.intprovider.ClampedIntProvider;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.PlacedFeature;
 import net.minecraft.world.gen.feature.PlacedFeatures;
 import net.minecraft.world.gen.feature.VegetationPlacedFeatures;
 import net.minecraft.world.gen.placementmodifier.BiomePlacementModifier;
+import net.minecraft.world.gen.placementmodifier.CountPlacementModifier;
 import net.minecraft.world.gen.placementmodifier.RarityFilterPlacementModifier;
 import net.minecraft.world.gen.placementmodifier.SquarePlacementModifier;
 
 public class JinericVegetationPlacedFeatures {
 	public static final RegistryKey<PlacedFeature> TREES_WISTFUL_FOREST = JinericPlacedFeatures.of("trees_wistful_forest");
 	public static final RegistryKey<PlacedFeature> CARVED_PUMPKIN_PATCH = JinericPlacedFeatures.of("carved_pumpkin_patch");
+	public static final RegistryKey<PlacedFeature> PATCH_ROSE = JinericPlacedFeatures.of("patch_rose");
+	public static final RegistryKey<PlacedFeature> PATCH_ROSES = JinericPlacedFeatures.of("patch_roses");
+	public static final RegistryKey<PlacedFeature> PATCH_BLOSSOMED_DANDELION = JinericPlacedFeatures.of("patch_blossomed_dandelion");
+	
 	
 	public static void bootstrap(Registerable<PlacedFeature> featureRegisterable) {
 		RegistryEntryLookup<ConfiguredFeature<?, ?>> registryEntryLookup = featureRegisterable.getRegistryLookup(RegistryKeys.CONFIGURED_FEATURE);
 		RegistryEntry<ConfiguredFeature<?, ?>> treesWistfulForest = registryEntryLookup.getOrThrow(JinericVegetationConfiguredFeatures.TREES_WISTFUL_FOREST);
 		RegistryEntry<ConfiguredFeature<?, ?>> carvedPumpkinPatch = registryEntryLookup.getOrThrow(JinericVegetationConfiguredFeatures.CARVED_PATCH_PUMPKIN);
+		RegistryEntry<ConfiguredFeature<?, ?>> patchRose = registryEntryLookup.getOrThrow(JinericVegetationConfiguredFeatures.PATCH_ROSE);
+		RegistryEntry<ConfiguredFeature<?, ?>> patchRoses = registryEntryLookup.getOrThrow(JinericVegetationConfiguredFeatures.PATCH_ROSES);
+		RegistryEntry<ConfiguredFeature<?, ?>> patchBlossomedDandelion = registryEntryLookup.getOrThrow(JinericVegetationConfiguredFeatures.PATCH_BLOSSOMED_DANDELION);
 		PlacedFeatures.register(featureRegisterable,
 				TREES_WISTFUL_FOREST,
 				treesWistfulForest,
@@ -28,13 +38,40 @@ public class JinericVegetationPlacedFeatures {
 						PlacedFeatures.createCountExtraModifier(10, 0.1F, 1)
 				)
 		);
-      PlacedFeatures.register(featureRegisterable,
-              CARVED_PUMPKIN_PATCH,
-              carvedPumpkinPatch,
-              RarityFilterPlacementModifier.of(300),
-              SquarePlacementModifier.of(),
-              PlacedFeatures.MOTION_BLOCKING_HEIGHTMAP,
-              BiomePlacementModifier.of()
-      );
+		PlacedFeatures.register(featureRegisterable,
+				CARVED_PUMPKIN_PATCH,
+				carvedPumpkinPatch,
+				RarityFilterPlacementModifier.of(300),
+				SquarePlacementModifier.of(),
+				PlacedFeatures.MOTION_BLOCKING_HEIGHTMAP,
+				BiomePlacementModifier.of()
+		);
+		PlacedFeatures.register(featureRegisterable,
+				PATCH_ROSE,
+				patchRose,
+				RarityFilterPlacementModifier.of(5),
+				SquarePlacementModifier.of(),
+				PlacedFeatures.MOTION_BLOCKING_HEIGHTMAP,
+				CountPlacementModifier.of(ClampedIntProvider.create(UniformIntProvider.create(-1, 3), 0, 3)),
+				BiomePlacementModifier.of()
+		);
+		PlacedFeatures.register(featureRegisterable,
+				PATCH_ROSES,
+				patchRoses,
+				RarityFilterPlacementModifier.of(9),
+				SquarePlacementModifier.of(),
+				PlacedFeatures.MOTION_BLOCKING_HEIGHTMAP,
+				CountPlacementModifier.of(ClampedIntProvider.create(UniformIntProvider.create(-1, 3), 0, 3)),
+				BiomePlacementModifier.of()
+		);
+		PlacedFeatures.register(featureRegisterable,
+				PATCH_BLOSSOMED_DANDELION,
+				patchBlossomedDandelion,
+				RarityFilterPlacementModifier.of(5),
+				SquarePlacementModifier.of(),
+				PlacedFeatures.MOTION_BLOCKING_HEIGHTMAP,
+				CountPlacementModifier.of(ClampedIntProvider.create(UniformIntProvider.create(-1, 3), 0, 3)),
+				BiomePlacementModifier.of()
+		);
 	}
 }
