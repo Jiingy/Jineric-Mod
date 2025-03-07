dbf999ef880eb2ed039389efcc6bbc9e036890ba
Jingy <69827559+Jiingy@users.noreply.github.com>
2025-03-06 18:35:04 -0600

    Fixed biomes not being data generated
    Signed-off-by: Jingy <69827559+Jiingy@users.noreply.github.com>

--- src/main/java/jingy/jineric/data/generators/JinericBuiltRegistryProvider.java
+++ src/main/java/jingy/jineric/data/generators/JinericBuiltRegistryProvider.java
@@ -16,6 +16,7 @@ public class JinericBuiltRegistryProvider extends FabricDynamicRegistryProvider 
 	protected void configure(RegistryWrapper.WrapperLookup registries, Entries entries) {
 		entries.addAll(registries.getOrThrow(RegistryKeys.CONFIGURED_FEATURE));
 		entries.addAll(registries.getOrThrow(RegistryKeys.PLACED_FEATURE));
+		entries.addAll(registries.getOrThrow(RegistryKeys.BIOME));
 		entries.addAll(registries.getOrThrow(RegistryKeys.PAINTING_VARIANT));
 	}
 	
