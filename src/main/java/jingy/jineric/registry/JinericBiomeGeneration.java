1a7e8e89ee291c9652f26b5eb830a6a394edfdd1
Jingy <69827559+Jiingy@users.noreply.github.com>
2025-04-03 20:24:23 -0500

    Wistful forest now replaces plains instead of forest
    When it replaced forest, the generation just looked patchy and bad. This can change later.

--- src/main/java/jingy/jineric/registry/JinericBiomeGeneration.java
+++ src/main/java/jingy/jineric/registry/JinericBiomeGeneration.java
@@ -7,8 +7,6 @@ import net.minecraft.world.biome.BiomeKeys;
 public class JinericBiomeGeneration {
 	
 	public static void registerBiomeGeneration() {
-		BiomePlacement.replaceOverworld(BiomeKeys.FOREST, JinericBiomeKeys.WISTFUL_FOREST, 0.15D);
-		BiomePlacement.replaceOverworld(BiomeKeys.WINDSWEPT_FOREST, JinericBiomeKeys.WISTFUL_FOREST, 0.12D);
-		BiomePlacement.replaceOverworld(BiomeKeys.BIRCH_FOREST, JinericBiomeKeys.WISTFUL_FOREST, 0.12D);
+		BiomePlacement.replaceOverworld(BiomeKeys.PLAINS, JinericBiomeKeys.WISTFUL_FOREST, 0.1D);
 	}
 }
