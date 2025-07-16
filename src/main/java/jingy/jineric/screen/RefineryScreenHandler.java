ced753d0f63bda7e79b32244056a7276105f9970
Jingy <69827559+Jiingy@users.noreply.github.com>
2025-07-16 15:31:58 -0500

    Updated RefineryScreenHandler to use furnace recipe book type due to custom ones breaking

--- src/main/java/jingy/jineric/screen/RefineryScreenHandler.java
+++ src/main/java/jingy/jineric/screen/RefineryScreenHandler.java
@@ -5,12 +5,17 @@ import jingy.jineric.recipe.JinericRecipePropertySet;
 import jingy.jineric.recipe.JinericRecipeTypes;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.Inventory;
+import net.minecraft.recipe.book.RecipeBookType;
 import net.minecraft.screen.AbstractFurnaceScreenHandler;
 import net.minecraft.screen.PropertyDelegate;
 
 public class RefineryScreenHandler extends AbstractFurnaceScreenHandler {
 	public RefineryScreenHandler(int syncId, PlayerInventory playerInventory) {
-		super(JinericScreenHandlerType.REFINERY_SCREEN_HANDLER, JinericRecipeTypes.REFINING, JinericRecipePropertySet.REFINERY_INPUT, JinericRecipeBookTypes.JINERIC_REFINERY, syncId, playerInventory);
+		super(JinericScreenHandlerType.REFINERY_SCREEN_HANDLER, JinericRecipeTypes.REFINING, JinericRecipePropertySet.REFINERY_INPUT,
+//  Broken in 1.21.7 - Hopefully shouldn't break anything...
+//				JinericRecipeBookTypes.JINERIC_REFINERY,
+				RecipeBookType.FURNACE,
+				syncId, playerInventory);
 	}
 	
 	public RefineryScreenHandler(int syncId, PlayerInventory playerInventory, Inventory inventory, PropertyDelegate propertyDelegate) {
