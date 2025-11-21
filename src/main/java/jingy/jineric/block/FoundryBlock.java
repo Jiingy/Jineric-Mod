621891bff68d7e8a625688cdd0ee21afb5c6760f
Jingy <69827559+Jiingy@users.noreply.github.com>
2025-11-20 21:54:35 -0600

    Updated collision shape of foundry block

--- src/main/java/jingy/jineric/block/FoundryBlock.java
+++ src/main/java/jingy/jineric/block/FoundryBlock.java
@@ -5,6 +5,7 @@ import jingy.jineric.block.entity.FoundryBlockEntity;
 import jingy.jineric.registry.JinericBlockEntityType;
 import net.minecraft.block.AbstractFurnaceBlock;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.block.entity.AbstractFurnaceBlockEntity;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
@@ -12,12 +13,21 @@ import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.stat.Stats;
+import net.minecraft.util.Util;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 public class FoundryBlock extends AbstractFurnaceBlock {
 	public static final MapCodec<FoundryBlock> CODEC = createCodec(FoundryBlock::new);
+	public static final VoxelShape FOUNDRY_SHAPE = Util.make(() -> {
+				VoxelShape bottom = FoundryBlock.createColumnShape(14.0, 0, 13.0);
+				VoxelShape top = FoundryBlock.createColumnShape(12.0, 13.0, 16.0);
+		return VoxelShapes.union(top, bottom);
+	});
 	
 	protected FoundryBlock(Settings settings) {
 		super(settings);
@@ -40,6 +50,11 @@ public class FoundryBlock extends AbstractFurnaceBlock {
 			player.openHandledScreen(foundryBlockEntity);
 			player.incrementStat(Stats.INTERACT_WITH_BLAST_FURNACE);
 		}
+	}
+	
+	@Override
+	protected VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+		return FOUNDRY_SHAPE;
 	}
 	
 	@Nullable
