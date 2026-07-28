524e25f5877596db38c48132f499159f4d45a9de
Jingy <jingysgaming@gmail.com>
2026-07-28 12:02:56 -0500

    Fixed SpreadingSnowyBlock mixin

--- src/main/java/jingy/jineric/mixin/fix/BlocksForGrassSurvivingMixin.java
+++ src/main/java/jingy/jineric/mixin/fix/BlocksForGrassSurvivingMixin.java
@@ -1,27 +1,32 @@
 package jingy.jineric.mixin.fix;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import jingy.jineric.block.JinericBlocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.LevelReader;
-import net.minecraft.world.level.block.SpreadingSnowyDirtBlock;
+import net.minecraft.world.level.block.SpreadingSnowyBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(SpreadingSnowyDirtBlock.class)
+@Mixin(SpreadingSnowyBlock.class)
 public abstract class BlocksForGrassSurvivingMixin {
 	
 	@Inject(
-			method = "canBeGrass",
-			at = @At(value = "HEAD"),
+			method = "canStayAlive",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/world/level/lighting/LightEngine;getLightBlockInto(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/Direction;I)I"
+			),
 			cancellable = true
 	)
-	private static void canSurviveIfJmGrassBlock(BlockState state, LevelReader world, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
-		BlockPos blockPos = pos.above();
-		BlockState blockState = world.getBlockState(blockPos);
-		if (blockState.is(JinericBlocks.GRASS_BLOCK)) {
+	private static void canSurviveIfJmGrassBlock(
+			BlockState state, LevelReader level, BlockPos pos, CallbackInfoReturnable<Boolean> cir,
+			@Local(type = BlockState.class, name = "aboveState") BlockState aboveState
+	) {
+		if (aboveState.is(JinericBlocks.GRASS_BLOCK)) {
 			cir.setReturnValue(true);
 		}
 	}
