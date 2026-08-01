d72ce9892aab3c72e7c3289434c671bf49528f0d
Jingy <jingysgaming@gmail.com>
2026-08-01 16:45:52 -0500

    Fixed outdated method override
    Signed-off-by: Jingy <jingysgaming@gmail.com>

--- src/client/java/net/jineric/jineric_mod/gui/screen/ingame/CrucibleScreen.java
+++ src/client/java/net/jineric/jineric_mod/gui/screen/ingame/CrucibleScreen.java
@@ -4,7 +4,7 @@ import jingy.jineric.base.JinericMain;
 import jingy.jineric.screen.CrucibleScreenHandler;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.network.chat.Component;
@@ -18,12 +18,11 @@ public class CrucibleScreen extends AbstractContainerScreen<CrucibleScreenHandle
 	public CrucibleScreen(CrucibleScreenHandler handler, Inventory inventory, Component title) {
 		super(handler, inventory, title);
 	}
-	
+
 	@Override
-	protected void renderBg(GuiGraphics context, float deltaTicks, int mouseX, int mouseY) {
+	public void extractBackground(GuiGraphicsExtractor graphics, int mouseX, int mouseY, float a) {
 		int x = this.leftPos;
 		int y = (this.height - this.imageHeight) / 2;
-		context.blit(RenderPipelines.GUI_TEXTURED, TEXTURE, x, y, 0.0F, 0.0F, this.imageWidth, this.imageHeight, 256, 256);
+		graphics.blit(RenderPipelines.GUI_TEXTURED, TEXTURE, x, y, 0.0F, 0.0F, this.imageWidth, this.imageHeight, 256, 256);
 	}
-	
 }
