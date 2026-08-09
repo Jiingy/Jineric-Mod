48fd1b5c4a2212b1a7a59062a2b39b4a966df6c8
Jingy <jingysgaming@gmail.com>
2026-08-09 15:35:17 -0500

    Fixed block color providers
    Signed-off-by: Jingy <jingysgaming@gmail.com>

--- src/client/java/net/jineric/jineric_mod/registry/JinericColorProviders.java
+++ src/client/java/net/jineric/jineric_mod/registry/JinericColorProviders.java
@@ -1,36 +1,52 @@
 package net.jineric.jineric_mod.registry;
 
 import jingy.jineric.block.JinericBlocks;
-import jingy.jineric.block.JmGrassBlock;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.BlockColorRegistry;
+import net.minecraft.client.color.block.BlockColors;
+import net.minecraft.client.color.block.BlockTintSource;
+import net.minecraft.client.color.block.BlockTintSources;
 import net.minecraft.client.renderer.BiomeColors;
+import net.minecraft.client.renderer.block.BlockAndTintGetter;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.GrassColor;
+import net.minecraft.world.level.block.state.BlockState;
+
+import java.util.List;
 
 @Environment(EnvType.CLIENT)
-public class JinericColorProviders {
-	
+public class JinericColorProviders extends BlockColors {
+
 	public static void registerColorProviders() {
-		// BLOCKS
-		ColorProviderRegistry.BLOCK.register(
-				((state, world, pos, tintIndex) -> {
-					boolean snowy = state.getValue(JmGrassBlock.SNOWY);
-					if (world != null && pos != null) {
-						if (snowy) {
-							return tintIndex > 0 ? -1 : BiomeColors.getAverageGrassColor(world, pos);
-						} else {
-							return tintIndex > 0 ? 1 : BiomeColors.getAverageGrassColor(world, pos);
-						}
-					} else {
-						return GrassColor.get(0.5D, 1.0D);
-					}
-				}), JinericBlocks.GRASS_BLOCK
+		BlockColorRegistry.register(
+				List.of(jmGrassBlock()), JinericBlocks.GRASS_BLOCK
 		);
-		ColorProviderRegistry.BLOCK.register((state, world, pos, tintIndex) ->
-				world != null && pos != null
-						? BiomeColors.getAverageGrassColor(world, pos)
-						: GrassColor.get(0.5D, 1.0D), JinericBlocks.JUNGLE_LADDER
+
+		BlockColorRegistry.register(
+				List.of(
+						BlockTintSources.grass()
+				),
+				JinericBlocks.JUNGLE_LADDER
 		);
+	}
+
+	public static BlockTintSource jmGrassBlock() {
+		return new BlockTintSource() {
+			@Override
+			public int color(final BlockState state) {
+				return GrassColor.getDefaultColor();
+			}
+
+			@Override
+			public int colorInWorld(final BlockState state, final BlockAndTintGetter level, final BlockPos pos) {
+				return BiomeColors.getAverageGrassColor(level, pos);
+			}
+
+			@Override
+			public int colorAsTerrainParticle(final BlockState state, final BlockAndTintGetter level, final BlockPos pos) {
+				return BiomeColors.getAverageGrassColor(level, pos);
+			}
+		};
 	}
 }
