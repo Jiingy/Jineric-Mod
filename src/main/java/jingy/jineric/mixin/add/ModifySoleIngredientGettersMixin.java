c7cccd461770f3a8cfd3da4f600c6fb164413286
Jingy <jingysgaming@gmail.com>
2026-08-09 11:29:12 -0500

    Updated ModifySoleIngredientGettersMixin
    Signed-off-by: Jingy <jingysgaming@gmail.com>

--- src/main/java/jingy/jineric/mixin/add/ModifySoleIngredientGettersMixin.java
+++ src/main/java/jingy/jineric/mixin/add/ModifySoleIngredientGettersMixin.java
@@ -5,12 +5,7 @@ import jingy.jineric.recipe.JinericRecipePropertySet;
 import jingy.jineric.recipe.JinericRecipeTypes;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.packs.resources.SimplePreparableReloadListener;
-import net.minecraft.world.item.crafting.RecipeAccess;
-import net.minecraft.world.item.crafting.RecipeManager;
-import net.minecraft.world.item.crafting.RecipeMap;
-import net.minecraft.world.item.crafting.RecipePropertySet;
-import net.minecraft.world.item.crafting.RecipeType;
-import net.minecraft.world.item.crafting.SingleItemRecipe;
+import net.minecraft.world.item.crafting.*;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
@@ -21,17 +16,17 @@ import java.util.Map;
 @Mixin(RecipeManager.class)
 public abstract class ModifySoleIngredientGettersMixin extends SimplePreparableReloadListener<RecipeMap> implements RecipeAccess {
 	@Mutable @Shadow @Final private static Map<ResourceKey<RecipePropertySet>, RecipeManager.IngredientExtractor> RECIPE_PROPERTY_SETS;
-	
+
+	@Shadow
+	private static RecipeManager.IngredientExtractor forSingleInput(RecipeType<? extends SingleItemRecipe> type) {
+		throw new UnsupportedOperationException("Implemented via mixin");
+	}
+
 	static {
 		RECIPE_PROPERTY_SETS = ImmutableMap.<ResourceKey<RecipePropertySet>, RecipeManager.IngredientExtractor>builder()
 				.putAll(RECIPE_PROPERTY_SETS)
 				.put(JinericRecipePropertySet.REFINERY_INPUT, forSingleInput(JinericRecipeTypes.REFINING))
 				.put(JinericRecipePropertySet.FOUNDRY_INPUT, forSingleInput(JinericRecipeTypes.FOUNDRY_SMELTING))
 				.build();
-	}
-	
-	@Shadow
-	private static RecipeManager.IngredientExtractor forSingleInput(RecipeType<? extends SingleItemRecipe> expectedType) {
-		return null;
 	}
 }
