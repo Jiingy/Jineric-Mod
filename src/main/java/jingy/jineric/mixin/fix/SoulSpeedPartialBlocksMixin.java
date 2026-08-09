18ca2c2825de945f2be5ec23843d70df0308b052
Jingy <jingysgaming@gmail.com>
2026-08-09 09:48:27 -0500

    Fixed SoulSpeedPartialBlocksMixin target
    Signed-off-by: Jingy <jingysgaming@gmail.com>

--- src/main/java/jingy/jineric/mixin/fix/SoulSpeedPartialBlocksMixin.java
+++ src/main/java/jingy/jineric/mixin/fix/SoulSpeedPartialBlocksMixin.java
@@ -2,26 +2,25 @@ package jingy.jineric.mixin.fix;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import net.minecraft.advancements.criterion.EntityPredicate;
+import net.minecraft.advancements.predicates.entity.MovementAffectedByPredicate;
 import net.minecraft.core.BlockPos;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-@Mixin(EntityPredicate.class)
+@Mixin(MovementAffectedByPredicate.class)
 public abstract class SoulSpeedPartialBlocksMixin {
+
 	@WrapOperation(
-			method = "matches(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/phys/Vec3;Lnet/minecraft/world/entity/Entity;)Z",
+			method = "matches",
 			at = @At(
 					value = "INVOKE",
 					target = "Lnet/minecraft/world/entity/Entity;getBlockPosBelowThatAffectsMyMovement()Lnet/minecraft/core/BlockPos;"
 			)
 	)
 	private BlockPos allowPartialBlocksToProvideSoulSpeed(Entity instance, Operation<BlockPos> original) {
-		if (instance.getBlockStateOn()
-				.is(BlockTags.SOUL_SPEED_BLOCKS)
-		) {
+		if (instance.getBlockStateOn().is(BlockTags.SOUL_SPEED_BLOCKS)) {
 			return instance.getOnPos();
 		} else {
 			return instance.getBlockPosBelowThatAffectsMyMovement();
