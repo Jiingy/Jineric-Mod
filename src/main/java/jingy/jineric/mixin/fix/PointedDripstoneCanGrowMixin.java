e564fafe4139c329747cbe6b5b87ead89aafe0c4
Jingy <jingysgaming@gmail.com>
2026-08-09 16:29:04 -0500

    Fixed pointed dripstone growth mixin
    Signed-off-by: Jingy <jingysgaming@gmail.com>

--- src/main/java/jingy/jineric/mixin/fix/PointedDripstoneCanGrowMixin.java
+++ src/main/java/jingy/jineric/mixin/fix/PointedDripstoneCanGrowMixin.java
@@ -1,47 +1,39 @@
 package jingy.jineric.mixin.fix;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import jingy.jineric.block.JinericBlocks;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Fallable;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.PointedDripstoneBlock;
-import net.minecraft.world.level.block.SimpleWaterloggedBlock;
 import net.minecraft.world.level.block.SlabBlock;
+import net.minecraft.world.level.block.SpeleothemBlock;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.SlabType;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(PointedDripstoneBlock.class)
-public abstract class PointedDripstoneCanGrowMixin extends Block implements Fallable, SimpleWaterloggedBlock {
-	@Shadow
-	@Final
-	public static BooleanProperty WATERLOGGED;
-	
-	public PointedDripstoneCanGrowMixin(Properties settings) {
-		super(settings);
-	}
-	
-	@Inject(
-			method = "canGrow(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/block/state/BlockState;)Z",
-			at = @At("HEAD"),
-			cancellable = true
+@Mixin(SpeleothemBlock.class)
+public abstract class PointedDripstoneCanGrowMixin {
+
+	@ModifyReturnValue(
+			method = "canGrow",
+			at = @At("RETURN")
 	)
-	private static void jineric$canGrow(BlockState dripstoneBlockState, BlockState waterState, CallbackInfoReturnable<Boolean> cir) {
-		//Checks if block is == to DRIPSTONE_SLAB which is water logged and type.BOTTOM
-		if (dripstoneBlockState.is(JinericBlocks.DRIPSTONE_SLAB)
-				&& (dripstoneBlockState.getValue(WATERLOGGED)
-				&& (dripstoneBlockState.getValue(SlabBlock.TYPE) == SlabType.BOTTOM))) {
-			cir.setReturnValue(true);
-		}
-		//Checks if block is == to DRIPSTONE_STAIRS
-		if (dripstoneBlockState.is((JinericBlocks.DRIPSTONE_STAIRS))) {
-			cir.setReturnValue(true);
-			
+	private boolean canGrowFromPartialBlocks(
+            boolean original,
+            final LevelReader level, final BlockPos pos
+    ) {
+		BlockState stateAbove = level.getBlockState(pos.above());
+		if (level.getBlockState(pos).getBlock() instanceof PointedDripstoneBlock) {
+			if (stateAbove.is(JinericBlocks.DRIPSTONE_SLAB) && stateAbove.getValue(SlabBlock.TYPE).equals(SlabType.BOTTOM)) {
+				return true;
+			} else if (stateAbove.is(JinericBlocks.DRIPSTONE_STAIRS)) {
+				return true;
+			}  else {
+				return original;
+			}
+		} else {
+			return original;
 		}
 	}
 }
