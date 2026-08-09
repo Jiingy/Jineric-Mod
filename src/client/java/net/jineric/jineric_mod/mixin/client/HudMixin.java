9d4b83b38a695aa3383d9e225b38e68d51697f51
Jingy <jingysgaming@gmail.com>
2026-08-09 10:34:55 -0500

    Renamed InGameHudMixin
    Signed-off-by: Jingy <jingysgaming@gmail.com>

--- src/client/java/net/jineric/jineric_mod/mixin/client/HudMixin.java
+++ src/client/java/net/jineric/jineric_mod/mixin/client/HudMixin.java
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Hud.class)
-public abstract class InGameHudMixin {
+public abstract class HudMixin {
 	
 	@Inject(
 			method = "extractItemHotbar",
