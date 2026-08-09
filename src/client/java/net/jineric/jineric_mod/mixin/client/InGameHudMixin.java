e8a4b34502beb715873822bd06d6f4542ec6a271
Jingy <jingysgaming@gmail.com>
2026-08-09 10:34:38 -0500

    Fixed target for InGameHudMixin
    Signed-off-by: Jingy <jingysgaming@gmail.com>

--- src/client/java/net/jineric/jineric_mod/mixin/client/InGameHudMixin.java
+++ src/client/java/net/jineric/jineric_mod/mixin/client/InGameHudMixin.java
@@ -3,8 +3,8 @@ package net.jineric.jineric_mod.mixin.client;
 import com.llamalad7.mixinextras.sugar.Local;
 import jingy.jineric.StaticMixinFields;
 import net.minecraft.client.DeltaTracker;
-import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
+import net.minecraft.client.gui.Hud;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Gui.class)
+@Mixin(Hud.class)
 public abstract class InGameHudMixin {
 	
 	@Inject(
