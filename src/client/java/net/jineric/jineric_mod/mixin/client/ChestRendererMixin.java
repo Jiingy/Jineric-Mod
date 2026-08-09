6e39714b6c5df90228b7291cabd0f828d59939db
Jingy <jingysgaming@gmail.com>
2026-08-09 18:10:09 -0500

    Updated mixins for chest textures; not working properly yet and causes crash!!
    Signed-off-by: Jingy <jingysgaming@gmail.com>

--- src/client/java/net/jineric/jineric_mod/mixin/client/ChestRendererMixin.java
+++ src/client/java/net/jineric/jineric_mod/mixin/client/ChestRendererMixin.java
@@ -2,13 +2,15 @@ package net.jineric.jineric_mod.mixin.client;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import jingy.jineric.block.WoodenChestBlock;
-import net.jineric.jineric_mod.block.entity.state.JinericChestBlockEntityRenderStateVariant;
+import jingy.jineric.block.WoodenTrappedChestBlock;
+import net.jineric.jineric_mod.renderer.blockentity.state.JmChestMaterialTypes;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.ChestRenderer;
 import net.minecraft.client.renderer.blockentity.state.ChestRenderState;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.LidBlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(ChestRenderer.class)
@@ -18,56 +20,52 @@ public abstract class ChestRendererMixin<T extends BlockEntity & LidBlockEntity>
 			method = "getChestMaterial",
 			at = @At(
 					value = "RETURN",
-					ordinal = 4
+					ordinal = 3
 			)
 	)
-	private static ChestRenderState.ChestMaterialType getIdsForJinericChests(ChestRenderState.ChestMaterialType original, BlockEntity entity, boolean xmasTextures) {
-		if (entity.getBlockState().getBlock() instanceof WoodenChestBlock woodenChestBlock) {
-			return switch (woodenChestBlock.getWoodType().name()) {
-//				case "oak" -> JinericChestBlockEntityRenderStateVariant.JINERIC_OAK;
-				case "spruce" -> JinericChestBlockEntityRenderStateVariant.SPRUCE;
-				case "birch" -> JinericChestBlockEntityRenderStateVariant.BIRCH;
-				case "jungle" -> JinericChestBlockEntityRenderStateVariant.JUNGLE;
-				case "acacia" -> JinericChestBlockEntityRenderStateVariant.ACACIA;
-				case "dark_oak" -> JinericChestBlockEntityRenderStateVariant.DARK_OAK;
-				case "mangrove" -> JinericChestBlockEntityRenderStateVariant.MANGROVE;
-				case "cherry" -> JinericChestBlockEntityRenderStateVariant.CHERRY;
-//				case "pale_oak" -> JinericChestBlockEntityRenderStateVariant.JINERIC_PALE_OAK;
-				case "bamboo" -> JinericChestBlockEntityRenderStateVariant.BAMBOO;
-				case "crimson" -> JinericChestBlockEntityRenderStateVariant.CRIMSON;
-				case "warped" -> JinericChestBlockEntityRenderStateVariant.WARPED;
-				default -> ChestRenderState.ChestMaterialType.REGULAR;
-			};
+	private static ChestRenderState.ChestMaterialType materialForJmTrappedChests(ChestRenderState.ChestMaterialType original, BlockEntity entity, boolean xmasTextures) {
+		if (entity.getBlockState().getBlock() instanceof WoodenTrappedChestBlock trappedChestBlock) {
+			return materialType(trappedChestBlock.getWoodType().name(), true);
 		} else {
 			return original;
 		}
+
 	}
-	
+
 	@ModifyReturnValue(
 			method = "getChestMaterial",
 			at = @At(
 					value = "RETURN",
-					ordinal = 2
+					ordinal = 4
 			)
 	)
-	private static ChestRenderState.ChestMaterialType idsForWoodenTrappedChests(ChestRenderState.ChestMaterialType original, BlockEntity entity, boolean xmasTextures) {
-		if (entity.getBlockState().getBlock() instanceof WoodenChestBlock woodenChestBlock) {
-			return switch (woodenChestBlock.getWoodType().name()) {
-//				case "oak" -> JinericChestBlockEntityRenderStateVariant.JINERIC_TRAPPED_OAK;
-				case "spruce" -> JinericChestBlockEntityRenderStateVariant.TRAPPED_SPRUCE;
-				case "birch" -> JinericChestBlockEntityRenderStateVariant.TRAPPED_BIRCH;
-				case "jungle" -> JinericChestBlockEntityRenderStateVariant.TRAPPED_JUNGLE;
-				case "acacia" -> JinericChestBlockEntityRenderStateVariant.TRAPPED_ACACIA;
-				case "dark_oak" -> JinericChestBlockEntityRenderStateVariant.TRAPPED_DARK_OAK;
-				case "mangrove" -> JinericChestBlockEntityRenderStateVariant.TRAPPED_MANGROVE;
-				case "cherry" -> JinericChestBlockEntityRenderStateVariant.TRAPPED_CHERRY;
-//				case "pale_oak" -> JinericChestBlockEntityRenderStateVariant.JINERIC_TRAPPED_PALE_OAK;
-				case "bamboo" -> JinericChestBlockEntityRenderStateVariant.TRAPPED_BAMBOO;
-				case "crimson" -> JinericChestBlockEntityRenderStateVariant.TRAPPED_CRIMSON;
-				case "warped" -> JinericChestBlockEntityRenderStateVariant.TRAPPED_WARPED;
-				default -> ChestRenderState.ChestMaterialType.TRAPPED;};
+	private static ChestRenderState.ChestMaterialType materialForJmChests(ChestRenderState.ChestMaterialType original, BlockEntity entity, boolean xmasTextures) {
+		if (entity.getBlockState().getBlock() instanceof WoodenChestBlock chestBlock) {
+			return materialType(chestBlock.getWoodType().name(), false);
 		} else {
 			return original;
 		}
+	}
+
+	@Unique
+    private static ChestRenderState.ChestMaterialType materialType(String woodType, boolean trapped) {
+		ChestRenderState.ChestMaterialType returnType;
+		returnType =  switch (woodType) {
+//			case "oak" -> JmChestMaterialTypes.CHEST_TRAPPED_OAK;
+			case "spruce" -> trapped ? JmChestMaterialTypes.TRAPPED_CHEST_SPRUCE : JmChestMaterialTypes.CHEST_SPRUCE;
+			case "birch" -> trapped ? JmChestMaterialTypes.TRAPPED_CHEST_BIRCH : JmChestMaterialTypes.CHEST_BIRCH;
+			case "jungle" -> trapped ? JmChestMaterialTypes.TRAPPED_CHEST_JUNGLE : JmChestMaterialTypes.CHEST_JUNGLE;
+			case "acacia" -> trapped ? JmChestMaterialTypes.TRAPPED_CHEST_ACACIA : JmChestMaterialTypes.CHEST_ACACIA;
+			case "dark_oak" -> trapped ? JmChestMaterialTypes.TRAPPED_CHEST_DARK_OAK : JmChestMaterialTypes.CHEST_DARK_OAK;
+			case "mangrove" -> trapped ? JmChestMaterialTypes.TRAPPED_CHEST_MANGROVE : JmChestMaterialTypes.CHEST_MANGROVE;
+			case "cherry" -> trapped ? JmChestMaterialTypes.TRAPPED_CHEST_CHERRY : JmChestMaterialTypes.CHEST_CHERRY;
+//			case "pale_oak" -> trapped ? JmChestMaterialTypes.TRAPPED_CHEST_PALE_OAK : JmChestMaterialTypes.CHEST_PALE_OAK;
+//			case "poplar" -> trapped ? JmChestMaterialTypes.TRAPPED_CHEST_POPLAR : JmChestMaterialTypes.CHEST_POPLAR;
+			case "bamboo" -> trapped ? JmChestMaterialTypes.TRAPPED_CHEST_BAMBOO : JmChestMaterialTypes.CHEST_BAMBOO;
+			case "crimson" -> trapped ? JmChestMaterialTypes.TRAPPED_CHEST_CRIMSON : JmChestMaterialTypes.CHEST_CRIMSON;
+			case "warped" -> trapped ? JmChestMaterialTypes.TRAPPED_CHEST_WARPED : JmChestMaterialTypes.CHEST_WARPED;
+			default -> trapped ? ChestRenderState.ChestMaterialType.TRAPPED : ChestRenderState.ChestMaterialType.REGULAR;
+		};
+		return returnType;
 	}
 }
--- src/client/java/net/jineric/jineric_mod/mixin/client/SheetsMixin.java
+++ src/client/java/net/jineric/jineric_mod/mixin/client/SheetsMixin.java
@@ -1,35 +1,54 @@
 package net.jineric.jineric_mod.mixin.client;
 
-import net.jineric.jineric_mod.block.entity.state.JinericChestBlockEntityRenderStateVariant;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import net.jineric.jineric_mod.render.JmSheets;
 import net.minecraft.client.renderer.Sheets;
 import net.minecraft.client.renderer.blockentity.state.ChestRenderState;
-import net.minecraft.client.resources.model.Material;
+import net.minecraft.client.resources.model.sprite.SpriteId;
 import net.minecraft.world.level.block.state.properties.ChestType;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import java.util.Map;
 
 @Mixin(Sheets.class)
 public abstract class SheetsMixin {
-
-	@Shadow private static Material chooseMaterial(ChestType chestType, Material material, Material material2, Material material3) {
-		return null;
-	}
-	
-	@Inject(
-			method = "chooseMaterial(Lnet/minecraft/client/renderer/blockentity/state/ChestRenderState$ChestMaterialType;Lnet/minecraft/world/level/block/state/properties/ChestType;)Lnet/minecraft/client/resources/model/Material;",
-			at = @At("HEAD"),
-			cancellable = true
+	//TODO: 26.2 fix / The switch cannot use a string
+	@ModifyReturnValue(
+			method = "chooseSprite",
+			at = @At("RETURN")
 	)
-	private static void accountForJinericChestTextures(ChestRenderState.ChestMaterialType variant, ChestType type, CallbackInfoReturnable<Material> cir) {
-		Map<ChestRenderState.ChestMaterialType, JinericChestBlockEntityRenderStateVariant> variantSpriteMap = JinericChestBlockEntityRenderStateVariant.VARIANT_TO_SPRITE_ID;
-		if (variant.name().contains("JINERIC_")) {
-			JinericChestBlockEntityRenderStateVariant stateVariant = variantSpriteMap.get(variant);
-			cir.setReturnValue(chooseMaterial(type, stateVariant.normal(), stateVariant.left(), stateVariant.right()));
+	private static SpriteId accountForJinericChestTextures(
+			SpriteId original,
+			final ChestRenderState.ChestMaterialType materialType, final ChestType type
+	) {
+		String materialTypeName = materialType.name();
+		System.out.println("materialType = " + materialType);
+		System.out.println("materialTypeName = " + materialTypeName);
+		if (materialTypeName.contains("JM")) {
+			return switch(materialTypeName) {
+				case "JM_WOODEN_CHEST_SPRUCE" -> JmSheets.CHEST_SPRUCE.select(type);
+				case "JM_WOODEN_CHEST_BIRCH" -> JmSheets.CHEST_BIRCH.select(type);
+				case "JM_WOODEN_CHEST_JUNGLE" -> JmSheets.CHEST_JUNGLE.select(type);
+				case "JM_WOODEN_CHEST_ACACIA" -> JmSheets.CHEST_ACACIA.select(type);
+				case "JM_WOODEN_CHEST_DARK_OAK" -> JmSheets.CHEST_DARK_OAK.select(type);
+				case "JM_WOODEN_CHEST_MANGROVE" -> JmSheets.CHEST_MANGROVE.select(type);
+				case "JM_WOODEN_CHEST_CHERRY" -> JmSheets.CHEST_CHERRY.select(type);
+				case "JM_WOODEN_CHEST_PALE_OAK" -> JmSheets.CHEST_PALE_OAK.select(type);
+				case "JM_WOODEN_CHEST_POPLAR" -> JmSheets.CHEST_POPLAR.select(type);
+				case "JM_WOODEN_CHEST_BAMBOO" -> JmSheets.CHEST_BAMBOO.select(type);
+				case "JM_WOODEN_TRAPPED_CHEST_SPRUCE" -> JmSheets.CHEST_TRAPPED_SPRUCE.select(type);
+				case "JM_WOODEN_TRAPPED_CHEST_BIRCH" -> JmSheets.CHEST_TRAPPED_BIRCH.select(type);
+				case "JM_WOODEN_TRAPPED_CHEST_JUNGLE" -> JmSheets.CHEST_TRAPPED_JUNGLE.select(type);
+				case "JM_WOODEN_TRAPPED_CHEST_ACACIA" -> JmSheets.CHEST_TRAPPED_ACACIA.select(type);
+				case "JM_WOODEN_TRAPPED_CHEST_DARK_OAK" -> JmSheets.CHEST_TRAPPED_DARK_OAK.select(type);
+				case "JM_WOODEN_TRAPPED_CHEST_MANGROVE" -> JmSheets.CHEST_TRAPPED_MANGROVE.select(type);
+				case "JM_WOODEN_TRAPPED_CHEST_CHERRY" -> JmSheets.CHEST_TRAPPED_CHERRY.select(type);
+				case "JM_WOODEN_TRAPPED_CHEST_PALE_OAK" -> JmSheets.CHEST_TRAPPED_PALE_OAK.select(type);
+				case "JM_WOODEN_TRAPPED_CHEST_POPLAR" -> JmSheets.CHEST_TRAPPED_POPLAR.select(type);
+				case "JM_WOODEN_TRAPPED_CHEST_BAMBOO" -> JmSheets.CHEST_TRAPPED_BAMBOO.select(type);
+				default -> Sheets.CHEST_REGULAR.select(type);
+			};
+		} else {
+			return original;
 		}
 	}
 }
