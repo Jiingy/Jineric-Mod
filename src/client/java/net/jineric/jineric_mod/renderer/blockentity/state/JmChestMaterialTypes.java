845b62daee5501e5b932d55f4af32c3af5909bf8
Jingy <jingysgaming@gmail.com>
2026-08-09 18:03:52 -0500

    Added custom chest material types
    Signed-off-by: Jingy <jingysgaming@gmail.com>

---
+++ src/client/java/net/jineric/jineric_mod/renderer/blockentity/state/JmChestMaterialTypes.java
@@ -0,0 +1,31 @@
+package net.jineric.jineric_mod.renderer.blockentity.state;
+
+import com.chocohead.mm.api.ClassTinkerers;
+import net.minecraft.client.renderer.blockentity.state.ChestRenderState;
+
+public class JmChestMaterialTypes {
+    public static final ChestRenderState.ChestMaterialType CHEST_SPRUCE = ClassTinkerers.getEnum(ChestRenderState.ChestMaterialType.class, "JM_WOODEN_CHEST_SPRUCE");
+    public static final ChestRenderState.ChestMaterialType CHEST_BIRCH = ClassTinkerers.getEnum(ChestRenderState.ChestMaterialType.class, "JM_WOODEN_CHEST_BIRCH");
+    public static final ChestRenderState.ChestMaterialType CHEST_JUNGLE = ClassTinkerers.getEnum(ChestRenderState.ChestMaterialType.class, "JM_WOODEN_CHEST_JUNGLE");
+    public static final ChestRenderState.ChestMaterialType CHEST_ACACIA = ClassTinkerers.getEnum(ChestRenderState.ChestMaterialType.class, "JM_WOODEN_CHEST_ACACIA");
+    public static final ChestRenderState.ChestMaterialType CHEST_DARK_OAK = ClassTinkerers.getEnum(ChestRenderState.ChestMaterialType.class, "JM_WOODEN_CHEST_DARK_OAK");
+    public static final ChestRenderState.ChestMaterialType CHEST_MANGROVE = ClassTinkerers.getEnum(ChestRenderState.ChestMaterialType.class, "JM_WOODEN_CHEST_MANGROVE");
+    public static final ChestRenderState.ChestMaterialType CHEST_CHERRY = ClassTinkerers.getEnum(ChestRenderState.ChestMaterialType.class, "JM_WOODEN_CHEST_CHERRY");
+//    public static final ChestRenderState.ChestMaterialType CHEST_PALE_OAK = ClassTinkerers.getEnum(ChestRenderState.ChestMaterialType.class, "JM_WOODEN_CHEST_PALE_OAK");
+//    public static final ChestRenderState.ChestMaterialType CHEST_POPLAR = ClassTinkerers.getEnum(ChestRenderState.ChestMaterialType.class, "JM_WOODEN_CHEST_POPLAR");
+    public static final ChestRenderState.ChestMaterialType CHEST_BAMBOO = ClassTinkerers.getEnum(ChestRenderState.ChestMaterialType.class, "JM_WOODEN_CHEST_BAMBOO");
+    public static final ChestRenderState.ChestMaterialType CHEST_CRIMSON = ClassTinkerers.getEnum(ChestRenderState.ChestMaterialType.class, "JM_WOODEN_CHEST_CRIMSON");
+    public static final ChestRenderState.ChestMaterialType CHEST_WARPED = ClassTinkerers.getEnum(ChestRenderState.ChestMaterialType.class, "JM_WOODEN_CHEST_WARPED");
+    public static final ChestRenderState.ChestMaterialType TRAPPED_CHEST_SPRUCE = ClassTinkerers.getEnum(ChestRenderState.ChestMaterialType.class, "JM_WOODEN_TRAPPED_CHEST_SPRUCE");
+    public static final ChestRenderState.ChestMaterialType TRAPPED_CHEST_BIRCH = ClassTinkerers.getEnum(ChestRenderState.ChestMaterialType.class, "JM_WOODEN_TRAPPED_CHEST_BIRCH");
+    public static final ChestRenderState.ChestMaterialType TRAPPED_CHEST_JUNGLE = ClassTinkerers.getEnum(ChestRenderState.ChestMaterialType.class, "JM_WOODEN_TRAPPED_CHEST_JUNGLE");
+    public static final ChestRenderState.ChestMaterialType TRAPPED_CHEST_ACACIA = ClassTinkerers.getEnum(ChestRenderState.ChestMaterialType.class, "JM_WOODEN_TRAPPED_CHEST_ACACIA");
+    public static final ChestRenderState.ChestMaterialType TRAPPED_CHEST_DARK_OAK = ClassTinkerers.getEnum(ChestRenderState.ChestMaterialType.class, "JM_WOODEN_TRAPPED_CHEST_DARK_OAK");
+    public static final ChestRenderState.ChestMaterialType TRAPPED_CHEST_MANGROVE = ClassTinkerers.getEnum(ChestRenderState.ChestMaterialType.class, "JM_WOODEN_TRAPPED_CHEST_MANGROVE");
+    public static final ChestRenderState.ChestMaterialType TRAPPED_CHEST_CHERRY = ClassTinkerers.getEnum(ChestRenderState.ChestMaterialType.class, "JM_WOODEN_TRAPPED_CHEST_CHERRY");
+//    public static final ChestRenderState.ChestMaterialType TRAPPED_CHEST_PALE_OAK = ClassTinkerers.getEnum(ChestRenderState.ChestMaterialType.class, "JM_WOODEN_TRAPPED_CHEST_PALE_OAK");
+//    public static final ChestRenderState.ChestMaterialType TRAPPED_CHEST_POPLAR = ClassTinkerers.getEnum(ChestRenderState.ChestMaterialType.class, "JM_WOODEN_TRAPPED_CHEST_POPLAR");
+    public static final ChestRenderState.ChestMaterialType TRAPPED_CHEST_BAMBOO = ClassTinkerers.getEnum(ChestRenderState.ChestMaterialType.class, "JM_WOODEN_TRAPPED_CHEST_BAMBOO");
+    public static final ChestRenderState.ChestMaterialType TRAPPED_CHEST_CRIMSON = ClassTinkerers.getEnum(ChestRenderState.ChestMaterialType.class, "JM_WOODEN_TRAPPED_CHEST_CRIMSON");
+    public static final ChestRenderState.ChestMaterialType TRAPPED_CHEST_WARPED = ClassTinkerers.getEnum(ChestRenderState.ChestMaterialType.class, "JM_WOODEN_TRAPPED_CHEST_WARPED");
+}
