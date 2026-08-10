0702fe73b11dfbd7de82c428203abf9509a80290
Jingy <jingysgaming@gmail.com>
2026-08-10 17:07:55 -0500

    Fixed missing ItemId names
    Signed-off-by: Jingy <jingysgaming@gmail.com>

--- src/main/java/jingy/jineric/references/JmItemIds.java
+++ src/main/java/jingy/jineric/references/JmItemIds.java
@@ -7,10 +7,10 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.Item;
 
 public class JmItemIds extends ItemIds {
-    public static final ResourceKey<Item> WOODEN_HELMET = create("");
-    public static final ResourceKey<Item> WOODEN_CHESTPLATE = create("");
-    public static final ResourceKey<Item> WOODEN_LEGGINGS = create("");
-    public static final ResourceKey<Item> WOODEN_BOOTS = create("");
+    public static final ResourceKey<Item> WOODEN_HELMET = create("wooden_helmet");
+    public static final ResourceKey<Item> WOODEN_CHESTPLATE = create("wooden_chestplate");
+    public static final ResourceKey<Item> WOODEN_LEGGINGS = create("wooden_leggings");
+    public static final ResourceKey<Item> WOODEN_BOOTS = create("wooden_boots");
 
     private static ResourceKey<Item> create(final String name) {
         return ResourceKey.create(Registries.ITEM, JinericMain.ofJineric(name));
