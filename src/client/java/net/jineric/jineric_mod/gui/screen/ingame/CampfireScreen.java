7a79c93fe04ae3a65ef80f375caff0f9caf36ef7
Jingy <jingysgaming@gmail.com>
2026-08-15 16:31:00 -0500

    Fixed campfire screen not calling super implementation
    Signed-off-by: Jingy <jingysgaming@gmail.com>

--- src/client/java/net/jineric/jineric_mod/gui/screen/ingame/CampfireScreen.java
+++ src/client/java/net/jineric/jineric_mod/gui/screen/ingame/CampfireScreen.java
@@ -52,6 +52,7 @@ public class CampfireScreen extends AbstractContainerScreen<CampfireScreenHandle
 
 	@Override
 	public void extractBackground(GuiGraphicsExtractor graphics, int mouseX, int mouseY, float a) {
+		super.extractBackground(graphics, mouseX, mouseY, a);
 		int x = this.leftPos;
 		int y = (this.height - this.imageHeight) / 2;
 		graphics.blit(RenderPipelines.GUI_TEXTURED, TEXTURE, x, y, 0.0F, 0.0F, this.imageWidth, this.imageHeight, 256, 256);
